d82a28fec67ed4a053b2b2217510c0cf64ab0586
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-09-28 02:30:50 +0530

    Time: 45 ms (89.51%), Space: 45.7 MB (42.96%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.java
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -1,32 +1,26 @@
 class MyCalendarTwo {
-
-    private TreeMap<Integer, Integer> map;    
-    
+    private List<int[]> Single;
+    private List<int[]> Double;
     public MyCalendarTwo() {
-        map = new TreeMap<>();
+        Single = new ArrayList<>();
+        Double = new ArrayList<>();
     }
     
     public boolean book(int start, int end) {
-        map.put(start, map.getOrDefault(start, 0) + 1);
-        map.put(end, map.getOrDefault(end, 0) - 1);
-        int count = 0;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            count += entry.getValue();
-            if(count > 2) {
-                map.put(start, map.get(start) - 1);
-                if(map.get(start) == 0) {
-                    map.remove(start);
-                }
-                map.put(end, map.get(end) + 1);
-                if(map.get(end) == 0) {
-                    map.remove(end);
-                }
-                return false;
+        for(int[] b : Double){
+            if(Math.max(b[0] , start ) < Math.min(b[1] , end)) return false;
+        }
+
+        for(int[] b : Single){
+            if(Math.max(b[0] , start ) < Math.min(b[1] , end)){
+                Double.add(new int[]{Math.max(b[0] , start) , Math.min(b[1] , end)});
             }
         }
+        Single.add(new int[]{start , end});
         return true;
     }
 }
+
 /**
  * Your MyCalendarTwo object will be instantiated and called as such:
  * MyCalendarTwo obj = new MyCalendarTwo();
