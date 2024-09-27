db620495d8b519d8e150b64ab193a936cb878adb
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-09-28 01:30:22 +0530

    Time: 64 ms (35.78%), Space: 45.7 MB (27.22%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,12 +1,18 @@
 class MyCalendar {
-    private List<int[]> books = new ArrayList<>();
+    private List<int[]> list;
+    public MyCalendar() {
+        list = new ArrayList<>();
+    }
+    
     public boolean book(int start, int end) {
-        for (int[] b : books)
-            if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
-        books.add(new int[]{ start, end });
+        for(int[] b : list){
+            if(Math.max(b[0] , start ) < Math.min(b[1] , end)) return false;
+        }
+        list.add(new int[]{start , end});
         return true;
     }
 }
+
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar obj = new MyCalendar();
