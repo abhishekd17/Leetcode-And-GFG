bd434b6a91a061722299bb1a67ddd71e0004a83d
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-09-30 22:56:16 +0530

    Time: 18 ms (17.76%), Space: 45.4 MB (86.63%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,6 +3,29 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
+        int prev[] = new int[m + 1];
+
+        prev[0] = 1;
+        for(int i = 1 ; i <= n ;i++){
+            int curr[] = new int[m + 1];
+            curr[0]=1;
+            for(int j = 1 ; j <= m ;j++){
+               if(s.charAt(i - 1) == t.charAt(j - 1)){
+                curr[j] = (prev[j - 1] + prev[j])%prime;
+               }else{
+                curr[j] = prev[j];
+               }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+}
+/*class Solution {
+    static int prime = (int) (Math.pow(10, 9) + 7);
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
         int dp[][] = new int[n + 1 ][m + 1];
 
         for(int i = 0 ; i <= n ;i++){
@@ -22,7 +45,7 @@ class Solution {
         }
         return dp[n][m];
     }
-}
+} */
 
 /*class Solution {
     public int numDistinct(String s, String t) {
