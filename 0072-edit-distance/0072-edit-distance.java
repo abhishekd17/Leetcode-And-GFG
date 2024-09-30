0d9d06d47a3413389e12bac96590110bbf02f25d
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-09-30 23:58:35 +0530

    Time: 5 ms (65.54%), Space: 44.7 MB (88.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,5 +1,29 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        for(int i = 0 ; i <= n ;i++){
+            dp[i][0] = i ;
+        }
+        for(int j = 0 ; j <= m ;j++){
+            dp[0][j] = j;
+        }
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ;j++){
+                if(word1.charAt(i - 1) == word2.charAt(j -1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j] , Math.min(dp[i-1][j-1] , dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
+/*class Solution {
+    public int minDistance(String word1, String word2) {
         int dp[][] = new int[word1.length()][word2.length()];
         for(int[] p : dp){
             Arrays.fill(p , -1);
@@ -21,7 +45,7 @@ class Solution {
         }
     }
 
-}
+}*/
 /*class Solution {
     public int minDistance(String word1, String word2) {
         return helper(word1 , word2 , word1.length() - 1 , word2.length() - 1);
