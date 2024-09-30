88dfc18586c5282c9e35116d8bb24c54c592df46
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-01 00:04:13 +0530

    Time: 4 ms (74.15%), Space: 44.8 MB (79.26%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,6 +2,31 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
+
+        int prev[] = new int[m + 1];
+        for(int i = 0 ; i <= m ; i++){
+            prev[i] = i;
+        }
+        for(int i = 1 ; i <= n ;i++){
+            int curr[] = new int[m + 1];
+            curr[0] = i ; 
+            for(int j = 1 ; j <= m ; j++){
+                if(word1.charAt(i - 1) == word2.charAt(j -1)){
+                    curr[j] = prev[j-1];
+                }else{
+                    curr[j] = 1 + Math.min(prev[j] , Math.min(prev[j-1] , curr[j-1]));
+                }
+            }
+            prev = curr;
+            }
+            return prev[m];
+        }
+    }
+
+/*class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
         int dp[][] = new int[n + 1][m + 1];
 
         for(int i = 0 ; i <= n ;i++){
@@ -21,7 +46,7 @@ class Solution {
         }
         return dp[n][m];
     }
-}
+}*/
 /*class Solution {
     public int minDistance(String word1, String word2) {
         int dp[][] = new int[word1.length()][word2.length()];
