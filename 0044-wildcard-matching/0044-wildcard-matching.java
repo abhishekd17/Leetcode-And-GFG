83279e1f09549be472d6ecddadf527a22b7da4fc
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-01 14:27:43 +0530

    Time: 24 ms (33.87%), Space: 44.7 MB (70.36%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,4 +1,47 @@
 class Solution {
+    private boolean isAllStars(String t , int i){
+        for(int j = 0 ; j <= i  ;j++){
+            if(t.charAt(j) != '*') return false;
+        }
+        return true;
+    }
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+
+        boolean dp[][] = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+
+        for(int i = 1; i<= n ;i++){
+            dp[i][0] = false;
+        }
+        for(int j = 1 ; j <= m ; j++){
+            if(isAllStars(p , j - 1)){
+                dp[0][j] = true;
+            }else{
+                dp[0][j] = false;
+            }
+        }
+
+        for(int i = 1 ; i <= n ;i++){
+            for(int j = 1 ; j <=m ;j++){
+                if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?'){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else{
+                    if(p.charAt(j - 1) == '*'){
+                        dp[i][j] = (dp[i - 1][j] || dp[i][j - 1]);
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
+
+
+/*class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
@@ -27,4 +70,4 @@ class Solution {
             }
         }
     }
-}
+} */
