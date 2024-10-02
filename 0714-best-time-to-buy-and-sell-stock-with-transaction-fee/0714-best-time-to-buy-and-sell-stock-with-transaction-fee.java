03976b2592589fad126ba7c62d3bf3b367269c0a
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-03 01:30:06 +0530

    Time: 18 ms (52.72%), Space: 56.3 MB (33.09%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,5 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
+        int n =  prices.length;
+
+        int[][] dp= new int[n + 1][2];
+
+        for(int i = n - 1; i >=0 ; i--){
+            for(int j = 0 ; j <= 1 ; j++){
+                if(j == 0 ){
+                    dp[i][j] = Math.max(-prices[i] + dp[i + 1 ][1] , dp[i + 1][0] );
+                }else{
+                    dp[i][j] = Math.max(prices[i] - fee + dp[i + 1 ][0] , dp[i + 1][1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
+
+/*class Solution {
+    public int maxProfit(int[] prices, int fee) {
         int dp[][]= new int[prices.length][2];
         for(int[] a : dp){
             Arrays.fill(a , -1);
@@ -17,4 +36,4 @@ class Solution {
         }
         return dp[ind][buy];
     }
-}
+} */
