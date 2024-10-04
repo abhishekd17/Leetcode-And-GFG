9487da128a871f6be136eb2ade7a652103066002
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-04 18:43:26 +0530

    Time: 81 ms (23.44%), Space: 77.2 MB (18.15%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,6 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+        int dp[][] =new int[n+1][n + 1];
+        for(int ind = n -1 ; ind >= 0 ; ind--){
+            for(int prev = ind - 1 ; prev >= -1 ; prev-- ){
+                int not_pick = dp[ind + 1][prev + 1];
+                int pick = 0;
+                if(prev == -1 || nums[ind] > nums[prev]){
+                    pick = 1 + dp[ind + 1][ind + 1];
+                }
+                dp[ind][prev + 1 ] = Math.max(pick  , not_pick);
+            }
+        }
+        return dp[0][0];
+    }
+}
+
+/*class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
         int dp[][] = new int[n][n + 1];
         for(int[] a : dp) Arrays.fill(a , -1);
         return helper(nums, 0 , -1 , dp);
@@ -18,4 +36,4 @@ class Solution {
         }
         return dp[ind][prev + 1]= Math.max(pick , not_pick);
     }
-}
+} */
