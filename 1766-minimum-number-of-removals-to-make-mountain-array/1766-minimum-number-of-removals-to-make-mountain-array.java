1bbf86300b7999e9172424a4851abb8ad1b64ecd
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-05 00:09:16 +0530

    Time: 40 ms (47.62%), Space: 44.5 MB (29.97%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int minimumMountainRemovals(int[] nums) {
         int n = nums.length;
         int dp1[] = new int[n];
@@ -48,5 +48,44 @@ class Solution {
             i++;
             j--;
         }
+    }
+}*/
+class Solution {
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        int dp1[] = new int[n];  // For Longest Increasing Subsequence (LIS)
+        int dp2[] = new int[n];  // For Longest Decreasing Subsequence (LDS)
+        
+        Arrays.fill(dp1, 1);
+        Arrays.fill(dp2, 1);
+        
+        // Compute LIS for each index
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && dp1[i] < dp1[j] + 1) {
+                    dp1[i] = dp1[j] + 1;
+                }
+            }
+        }
+
+        // Compute LDS for each index
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (nums[i] > nums[j] && dp2[i] < dp2[j] + 1) {
+                    dp2[i] = dp2[j] + 1;
+                }
+            }
+        }
+
+        // Find the longest mountain subsequence
+        int maxMountainLength = 0;
+        for (int i = 1; i < n - 1; i++) {  // Peak cannot be at index 0 or n-1
+            if (dp1[i] > 1 && dp2[i] > 1) {  // Both increasing and decreasing
+                maxMountainLength = Math.max(maxMountainLength, dp1[i] + dp2[i] - 1);
+            }
+        }
+
+        // Minimum removals is total length minus the longest mountain subsequence
+        return n - maxMountainLength;
     }
 }
