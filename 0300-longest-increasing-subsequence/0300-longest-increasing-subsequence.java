903d3d1ec95dab7d64025917c7c6f346d6434dd4
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-04 18:23:54 +0530

    Time: 406 ms (6.96%), Space: 79.3 MB (10.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,18 +3,18 @@ class Solution {
         int n = nums.length;
         int dp[][] = new int[n][n + 1];
         for(int[] a : dp) Arrays.fill(a , -1);
-        return helper(nums, n - 1 , -1 , dp);
+        return helper(nums, 0 , -1 , dp);
     }
     private int helper(int[] nums ,int ind , int prev , int dp[][]){
         // base case
-        if(ind < 0) return 0;
+        if(ind ==  nums.length) return 0;
         if(dp[ind][prev + 1] != -1) return dp[ind][prev + 1];
         // not_pick
-        int not_pick = helper(nums , ind - 1 , prev, dp);
+        int not_pick = helper(nums , ind + 1 , prev, dp);
         // pick
         int pick = Integer.MIN_VALUE;
-        if(prev == -1 || ( nums[prev ] > nums[ind])){
-            pick = 1+ helper(nums , ind - 1 , ind, dp);
+        if(prev == -1 || ( nums[prev ] < nums[ind])){
+            pick = 1+ helper(nums , ind + 1 , ind, dp);
         }
         return dp[ind][prev + 1]= Math.max(pick , not_pick);
     }
