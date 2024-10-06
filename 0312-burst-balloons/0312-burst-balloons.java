661cc91d27b1e0d716a5a274e801b987bffd70ea
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-07 02:25:02 +0530

    Time: 32 ms (92.31%), Space: 42.3 MB (90.63%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,14 +1,47 @@
 class Solution {
     public int maxCoins(int[] nums) {
         int arr[] = new int[nums.length + 2];
-        arr[0] = 1;
-        arr[arr.length - 1] = 1;
         int n = arr.length;
+
+        arr[0] = 1;
+        arr[n - 1] = 1;
+
+        for(int i = 0 ; i < nums.length ; i++ ){
+            arr[i + 1] = nums[i];
+        }
+
         int dp[][] = new int[n][n];
-        for(int[] a : dp) Arrays.fill(a , -1);
+
+        for(int i = nums.length ; i >= 1 ; i--){
+            for(int j = i ; j <= nums.length ; j++){
+                if(i > j ) dp[i][j] = 0;
+                int maxi = Integer.MIN_VALUE;
+                for(int k = i ; k <= j ;k++){
+                    int cost = (arr[j + 1] * arr[i - 1] * arr[k] ) + dp[i][k - 1] + dp[k + 1][j];
+                    maxi = Math.max(maxi ,cost);
+                }
+                dp[i][j] = maxi ; 
+            }
+        }
+        return dp[1][nums.length];
+    }
+}
+
+/*class Solution {
+    public int maxCoins(int[] nums) {
+        int arr[] = new int[nums.length + 2];
+        arr[0] = 1;
+
+        arr[arr.length - 1] = 1;
+
+        int n = arr.length;
+
         for(int i = 0 ; i < nums.length ; i++){
             arr[i + 1]  = nums[i];
         }
+
+        int dp[][] = new int[n][n];
+        for(int[] a : dp) Arrays.fill(a , -1);
         return helper(arr , 1 , nums.length , dp );
     }
     private int helper(int arr[] , int  l , int r , int[][] dp ){
@@ -21,4 +54,4 @@ class Solution {
         }
         return dp[l][r];
     }
-}
+} */
