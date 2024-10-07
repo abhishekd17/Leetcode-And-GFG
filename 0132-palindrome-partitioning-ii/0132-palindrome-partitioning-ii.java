901a60ecb79f537178a90342d1452f18b61a4ab9
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-07 16:36:26 +0530

    Time: 29 ms (86.88%), Space: 46.5 MB (51.72%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,5 +1,51 @@
 class Solution {
     public int minCut(String s) {
+        int n = s.length();
+        int cut[] = new int[n];
+        boolean[][] p = new boolean[n][n];
+
+        for(int i = 0 ; i < n ; i++){
+            int mini = i ;
+            for(int j = 0 ; j <= i ;j++){
+                if(s.charAt(i) == s.charAt(j) && (i - j  < 3 || p[j + 1][i - 1])){
+                    p[j][i] = true;
+                    mini = j == 0 ? 0  : Math.min(mini , cut[j - 1] + 1);
+                }
+            }
+            cut[i] = mini;
+        }
+        return cut[n-1];
+    }
+}
+
+/*class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int dp[] = new int[n + 1];
+        for(int i = n - 1; i >= 0 ; i--){
+            int mini = Integer.MAX_VALUE;
+            for(int j = i ; j < n ; j++){
+                if(isPalindrome(s.substring(i , j + 1))){
+                    int cnt = 1 + dp[j + 1];
+                    mini = Math.min(mini , cnt);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0] - 1;
+    }
+     private boolean isPalindrome(String s){
+        int i = 0 ;
+        int j = s.length() - 1;
+        while(i <= j){
+            if(s.charAt(i++) != s.charAt(j--)) return false;
+        }
+        return true;
+    }
+}*/
+
+/*class Solution {
+    public int minCut(String s) {
         int dp[] = new int[s.length()];
         Arrays.fill(dp ,-1);
         return helper(0 ,  s , dp) - 1;
@@ -26,7 +72,7 @@ class Solution {
     }
 }
 
-
+*/
 
 /*class Solution {
     public int minCut(String s) {
