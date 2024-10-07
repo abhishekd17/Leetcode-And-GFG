3acad6b6cdbb84965f2056102daeff9f6d6fad9d
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-07 18:35:11 +0530

    Time: 99 ms (35.92%), Space: 46 MB (67.61%) - LeetHub

--- 1871-palindrome-partitioning-iv/1871-palindrome-partitioning-iv.java
+++ 1871-palindrome-partitioning-iv/1871-palindrome-partitioning-iv.java
@@ -12,11 +12,9 @@ class Solution {
         }
        }
 
-      for(int i = 0 ; i < n - 2 ; i++){
-        if(p[0][i]){
-            for(int j = i + 1 ; j < n  - 1 ; j++){
-                if(p[i+1][j] && p[j + 1][n - 1]) return true;
-            }
+      for(int i = 1 ; i < n - 1 ; i++){
+        for(int j = i ; j < n - 1 ; j++){
+            if(p[0][i - 1] && p[i][j] && p[j + 1][n - 1]) return true;
         }
       }
       return false;
