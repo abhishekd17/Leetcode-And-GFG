e6fb63102e6fe1d5246ee158c62f0aff2e5e7e84
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-08 11:38:33 +0530

    Time: 7 ms (88.36%), Space: 60.5 MB (5.66%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -2,6 +2,25 @@ class Solution {
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
+        
+        int dp[][] = new int[n + 1][m + 1];
+        int max = 0;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(matrix[i - 1][j - 1] =='1'){
+                    dp[i][j] = Math.min(dp[i-1][j-1] , Math.min(dp[i-1][j] , dp[i][j-1])) + 1;
+                    max = Math.max(max , dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
+
+/*class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
         int height[] = new int[m];
         int maxArea = 0;
         for(int i = 0 ; i < n ; i++){
@@ -34,4 +53,4 @@ class Solution {
         }
         return maxans;
     }
-}
+} */
