bf5cefab2d8e751327f1d20336479ce77a24d37f
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-08 13:15:31 +0530

    Time: 17 ms (20.96%), Space: 45.6 MB (34.02%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
@@ -3,6 +3,45 @@ class Solution {
         int n = mat.length;
         int m = mat[0].length;
 
+        int height[] = new int[m];
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                height[j] = mat[i][j] == 0 ? 0 : height[j] + 1;
+            }
+            ans += helper(height);
+        }
+        return ans;
+    }
+
+    private int helper(int height[]){
+        int sum[] = new int[height.length];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0 ; i < height.length ; i++){
+            while(!st.isEmpty() && height[st.peek()] >= height[i]) st.pop();
+
+            if(!st.isEmpty()){
+                int preIndex = st.peek();
+                sum[i] = sum[preIndex];
+                sum[i] += height[i] * (i - preIndex);
+            }else{
+                sum[i] = height[i] * (i + 1);
+            }
+            st.push(i);
+        }
+        int res = 0;
+        for(int s : sum) res += s;
+        return res;
+    }
+}
+
+
+/*class Solution {
+    public int numSubmat(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
         int height[] =  new int[m];
 
         int res = 0;
@@ -17,4 +56,4 @@ class Solution {
         }
         return res;
     }
-}
+} */
