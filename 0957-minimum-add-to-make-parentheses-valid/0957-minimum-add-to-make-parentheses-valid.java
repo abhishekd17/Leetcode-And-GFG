662178da44852c273fb405cff8c22020c70e9fb5
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-10-09 18:51:48 +0530

    Time: 1 ms (58.43%), Space: 41.1 MB (90.31%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,5 +1,18 @@
 class Solution {
     public int minAddToMakeValid(String s) {
+        int left = 0;
+        int right = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '(') right++;
+            else if(right > 0) right--;
+            else left++;
+        }
+        return left + right;
+    }  
+}
+
+/*class Solution {
+    public int minAddToMakeValid(String s) {
         int cnt= 0;
         Stack<Character> st = new Stack<>();
         for(char c : s.toCharArray()){
@@ -9,4 +22,4 @@ class Solution {
         }
         return cnt + st.size();
     }
-}
+} */
