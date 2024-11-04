333615d191c84e2443b023cf3bb12fe0f5d2af75
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-11-04 13:33:09 +0530

    Time: 17 ms (80.51%), Space: 45.9 MB (59.79%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,5 +1,27 @@
 class Solution {
     public String compressedString(String word) {
+        StringBuilder ans = new StringBuilder();
+
+        int cnt = 1;
+        int n = word.length();
+        char ch = word.charAt(0);
+
+        for(int i = 1 ; i < n ; i++){
+            if(word.charAt(i) == ch && cnt < 9){
+                cnt++;
+            }else{
+                ans.append(cnt).append(ch);
+                ch = word.charAt(i);
+                cnt = 1;
+            }
+        }
+        ans.append(cnt).append(ch);
+        return ans.toString();
+    }
+}
+
+/*class Solution {
+    public String compressedString(String word) {
         int n = word.length();
         StringBuilder sb =new StringBuilder();
         for(int i = 0; i < n ;){
@@ -14,4 +36,4 @@ class Solution {
         }
         return sb.toString();
     }
-}
+} */
