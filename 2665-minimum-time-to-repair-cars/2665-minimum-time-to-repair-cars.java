fac32e5588c0b46b7f7474ae97ce6619f7ac54cf
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-11-15 10:50:18 +0530

    Time: 43 ms (65.42%), Space: 56.6 MB (8.12%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.java
@@ -1,5 +1,23 @@
 class Solution {
     public long repairCars(int[] ranks, int cars) {
+        long l = 1 , r = 1L*ranks[0] * cars * cars;
+        while(l < r){
+            long mid = l + (r - l) / 2 , rep_cars = 0;
+            for(int rr : ranks){
+                rep_cars += (long)Math.sqrt(mid / rr); 
+            }
+            if(rep_cars < cars){
+                l = mid + 1;
+            }else{
+                r = mid;
+            } 
+        }
+        return l;
+    }
+}
+
+/*class Solution {
+    public long repairCars(int[] ranks, int cars) {
         int n = ranks.length;
         long l = 0; 
         long r = (long)1e14;
@@ -17,4 +35,4 @@ class Solution {
         }
         return l;
     }
-}
+} */
