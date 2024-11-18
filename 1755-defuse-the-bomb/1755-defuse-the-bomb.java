d7e6caf8357362294f12d4e119ddb3a6065b6ef9
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-11-18 19:37:23 +0530

    Time: 0 ms (100%), Space: 42.5 MB (8.63%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,6 +1,29 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
+        int ans[] = new int[n];
+        if(k == 0) return ans;
+        int start = 1;
+        int end = k;
+        int sum = 0;
+        if(k < 0){
+            k = -k;
+            start = n -k;
+            end = n -1;
+        }
+        for(int i = start ; i <= end ;i++) sum += code[i];
+        for(int i = 0 ; i < n ; i++){
+            ans[i] = sum;
+            sum -= code[(start++) % n];
+            sum += code[(++end) % n];
+        }
+        return ans;
+    }
+}
+
+/*class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
         if(k == 0){
             Arrays.fill(code , 0);
             return code;
@@ -26,4 +49,4 @@ class Solution {
         }
         return ans;
     }
-}
+} */
