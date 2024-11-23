03f8a90f2aeaa94a2dd55de07bde46c926a3b826
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-11-23 16:36:17 +0530

    Time: 7 ms (79.67%), Space: 79.7 MB (36.21%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.java
+++ 1972-rotating-the-box/1972-rotating-the-box.java
@@ -2,18 +2,15 @@ class Solution {
     public char[][] rotateTheBox(char[][] box) {
         int n = box.length;
         int m = box[0].length;
-        char ans[][] = new char[n][m];
-        for(char[] c : ans){
-            Arrays.fill(c , '.');
-        }
+        
         for(int i = 0 ; i  < n ; i++){
             int emptyIndex = m - 1;
             for(int j = m - 1 ; j >= 0 ; j--){
                 if(box[i][j] == '*'){
-                    ans[i][j] = '*';
                     emptyIndex = j - 1;
                 }else if (box[i][j] == '#'){
-                    ans[i][emptyIndex] = '#';
+                    box[i][j] = '.';
+                    box[i][emptyIndex] = '#';
                     emptyIndex--;
                 }
             }
@@ -21,7 +18,7 @@ class Solution {
         char rotatedBox[][] = new char[m][n];
         for(int i = 0 ; i < n ; i++){
             for(int j = 0; j < m ; j++){
-                rotatedBox[j][n - i - 1] = ans[i][j];
+                rotatedBox[j][n - i - 1] = box[i][j];
             }
         }
         return rotatedBox;
