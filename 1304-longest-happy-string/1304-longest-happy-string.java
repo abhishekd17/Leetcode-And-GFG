0a0ccc3f71c7b2d6a447a8422235d898dab5366b
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-17 23:52:16 +0530

    Time: 1 ms (76.31%), Space: 41.1 MB (31.98%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.java
+++ 1304-longest-happy-string/1304-longest-happy-string.java
@@ -1,16 +1,51 @@
 class Solution {
-    String generate(int a, int b, int c, String aa, String bb, String cc) {
-    if (a < b)
-        return generate(b, a, c, bb, aa, cc);
-    if (b < c)
-        return generate(a, c, b, aa, cc, bb);
-    if (b == 0)
-        return aa.repeat(Math.min(2, a));
-    int use_a = Math.min(2, a), use_b = a - use_a >= b ? 1 : 0; 
-    return aa.repeat(use_a) + bb.repeat(use_b) +
-        generate(a - use_a, b - use_b, c, aa, bb, cc);    
-}
-public String longestDiverseString(int a, int b, int c) {
-    return generate(a, b, c, "a", "b", "c");
-}
+    public String longestDiverseString(int a, int b, int c) {
+    PriorityQueue<int[]> pq = new PriorityQueue<>((p, q) -> Integer.compare(q[1], p[1]));
+        if(a > 0 ) pq.offer(new int[]{0 , a});
+        if(b > 0 ) pq.offer(new int[]{1 , b});
+        if(c > 0 ) pq.offer(new int[]{2 , c});
+        StringBuilder sb = new StringBuilder();
+        while(pq.size() > 1){
+            int top1[] = pq.poll();
+            int top2[] = pq.poll();
+            if(top1[1] >= 2){
+                sb.append(String.valueOf((char)('a' + top1[0])).repeat(2));
+                top1[1] -= 2;
+            }else{
+                sb.append((char)('a' + top1[0]));
+                top1[1] -= 1;
+            }
+            if(top2[1] >= 2 && top1[1] < top2[1]){
+                sb.append((char)('a' + top2[0]));
+                sb.append((char)('a' + top2[0]));
+                top2[1] -= 2;
+            }else{
+                sb.append((char)('a' + top2[0]));
+                top2[1] -= 1;
+            }
+
+            if(top1[1] > 0) pq.offer(top1);
+            if(top2[1] > 0) pq.offer(top2);
+        }
+
+        if(!pq.isEmpty()){
+            int top[] = pq.peek();
+            if(sb.length() > 0 && sb.charAt(sb.length() - 1) != (char)('a' + top[0])){
+                if(top[1] >= 2){
+                sb.append((char)('a' + top[0]));
+                sb.append((char)('a' + top[0]));
+                }else{
+                sb.append((char)('a' + top[0]));
+                }
+            }else if(sb.length() == 0){
+                if(top[1] >= 2){
+                sb.append((char)('a' + top[0]));
+                sb.append((char)('a' + top[0]));
+                }else{
+                sb.append((char)('a' + top[0]));
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
