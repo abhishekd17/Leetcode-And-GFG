8e8dfc4f1dac0973c77e85c08168e8e7d183662c
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-18 15:11:32 +0530

    Time: 4 ms (40.47%), Space: 44.7 MB (6.13%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,6 +1,25 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
+        int nse[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = n -1 ; i >= 0 ; i--){
+            while(!st.isEmpty() && st.peek() > prices[i]){
+                st.pop();
+            }
+            nse[i] = st.isEmpty() ? 0 : st.peek();
+            st.push(prices[i]);
+        }
+        for(int i = 0 ; i < n ; i++){
+            prices[i] = prices[i] - nse[i];
+        }
+        return prices;
+    }
+}
+
+/*class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
         for(int i = 0 ; i < n ; i++){
             for(int j = i + 1 ; j < n ; j++){
                 if(prices[j] <= prices[i]){
@@ -11,4 +30,4 @@ class Solution {
         }
         return prices;
     }
-}
+} */
