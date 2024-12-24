c9ef0f8f9d885aac95389e3268e7cce0972069fb
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-25 02:22:05 +0530

    Time: 9 ms (80.98%), Space: 44.1 MB (80.69%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,4 +1,111 @@
+class DisjointSet{
+    int parent[];
+    int rank[];
+    int size[];
+    public DisjointSet(int n ){
+        this.parent = new int[n];
+        this.rank = new int[n];
+        this.size =new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            rank[i] = 0;
+            size[i] = 1;
+            parent[i] = i;
+        }
+    }
+    int findParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionByRank(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        }else if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }else{
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+    
+    void unionBySize(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_u == ulp_v) return ;
+        
+        if(size[ulp_u] > size[ulp_v]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+    
+}
 class Solution {
+    public int swimInWater(int[][] grid) {
+        int n = grid.length;
+
+        DisjointSet ds = new DisjointSet(n * n);
+
+        int vis[][] = new int[n][n];
+        int locations[][] = new int[n * n][2];
+        for(int i = 0 ; i  < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                locations[grid[i][j]] = new int[]{i,j};
+            }
+        }
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+
+        for(int time = 0 ; time < n * n ; time++){
+            int x = locations[time][0];
+            int y = locations[time][1];
+            vis[x][y] = 1;
+            for(int k = 0 ; k < 4 ; k++){
+                int newRow = x + drow[k];
+                int newCol = y + dcol[k];
+                if(isInBounds(newRow , newCol , n) && vis[newRow][newCol] == 1){
+                    ds.unionBySize(x * n + y , newRow * n + newCol); 
+                }
+            }
+            if(ds.findParent(0) == ds.findParent(n * n - 1)){
+                return time;
+            }
+        }
+        return n * n - 1;
+    }
+    private boolean isInBounds(int row , int col , int n){
+        return row >= 0 && row < n && col >=0 && col < n;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid.length;
 
@@ -31,4 +138,4 @@ class Solution {
         }
         return -1;
     }
-}
+} */
