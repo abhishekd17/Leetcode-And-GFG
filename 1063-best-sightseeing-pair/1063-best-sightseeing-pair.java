9740b1c43b4821b0a0e37c825cae2db87c9ff1e6
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-27 15:13:30 +0530

    Time: 4 ms (89.46%), Space: 54 MB (12.97%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -1,27 +1,21 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
         int n = values.length;
-        int ans1 = 0;
-        int ans2 = 0;
+        int ans = 0;
 
-        int max1 = 0, max2 = 0, start = 0, end = 0;
+        int max = 0;
 
         for (int i = 0; i < n; i++) {
-            // Calculate the score using the current maximum (max1) and the current index
             if (i > 0) {
-                ans1 = Math.max(ans1, max1 + values[i] - i);
+                ans = Math.max(ans, max + values[i] - i);
             }
 
-            // Update max1 for the next iteration
-            if (values[i] + i > max1) {
-                max1 = values[i] + i;
-                start = i;
+            if (values[i] + i > max) {
+                max = values[i] + i;
             }
 
-            // Debugging output to verify logic
-            System.out.println(max1 + " " + values[i] + " " + start + " " + end + " ");
         }
-        return ans1; // Return the maximum score
+        return ans;
     }
 }
 
