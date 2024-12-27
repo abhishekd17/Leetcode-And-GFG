3dd9b4a22cbe8ade6f029a2f5c64b8e94ecc96bb
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-27 23:37:10 +0530

    Time: 1 ms (8.22%), Space: 41.5 MB (52.94%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null ) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int ans = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0 ; i < size ; i++){
+                TreeNode node = q.poll();
+                if(node.left != null){
+                    q.offer(node.left);
+                    if(node.left.left == null && node.left.right == null){
+                        ans += node.left.val;
+                    } 
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
