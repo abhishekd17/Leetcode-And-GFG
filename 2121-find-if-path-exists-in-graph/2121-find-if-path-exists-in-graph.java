64e073512dcbbb26f4f39c3e6183d80555106fee
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-28 15:45:05 +0530

    Time: 15 ms (95.21%), Space: 157.1 MB (63.61%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -1,4 +1,65 @@
+class DisjointSet{
+    int parent[];
+    int rank[];
+    int size[];
+    public DisjointSet(int n ){
+        this.parent = new int[n];
+        this.rank = new int[n];
+        this.size =new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            rank[i] = 0;
+            size[i] = 1;
+            parent[i] = i;
+        }
+    }
+    int findParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionByRank(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        }else if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }else{
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+    
+    void unionBySize(int u , int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_u == ulp_v) return ;
+        
+        if(size[ulp_u] > size[ulp_v]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+    
+}
 class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        DisjointSet ds = new DisjointSet(n);
+        for(int[] edge : edges){
+            ds.unionBySize(edge[0] , edge[1]);
+        }
+        return ds.findParent(source) == ds.findParent(destination);
+    }
+}
+
+/*class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> list = new ArrayList<>();
         for(int i =  0;  i < n ; i++){
@@ -25,7 +86,7 @@ class Solution {
                 }
             }
         } 
-        // System.out.println(vis[source] + " " + vis[destination]);
+        System.out.println(vis[node] + " " + vis[destination]);
         return false;
     }
-}
+}*/
