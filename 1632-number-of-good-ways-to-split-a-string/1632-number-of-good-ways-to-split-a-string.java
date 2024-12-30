87c91523c7915145255a4fec26dee1bf81ffe60e
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-31 02:40:56 +0530

    Time: 39 ms (28.87%), Space: 45.1 MB (39.95%) - LeetHub

---
+++ 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSplits(String s) {
+        HashMap<Character , Integer> map1 = new HashMap<>();
+        HashMap<Character , Integer> map2 = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map2.put(c , map2.getOrDefault(c , 0) + 1);
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            map1.put(ch , map1.getOrDefault(ch , 0) + 1);
+            map2.put(ch , map2.getOrDefault(ch , 0) - 1);
+            if(map2.get(ch) == 0) {
+                map2.remove(ch);
+            }
+            if(map1.size() == map2.size()) ans++;
+        }
+        return ans;
+    }
+}
