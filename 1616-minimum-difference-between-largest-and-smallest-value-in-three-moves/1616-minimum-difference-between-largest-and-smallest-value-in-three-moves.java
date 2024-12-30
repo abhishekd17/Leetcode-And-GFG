3d739c9f9ab4b4d2fb1b73939a1e558ec74526d0
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2024-12-31 02:24:30 +0530

    Time: 16 ms (94.91%), Space: 56.5 MB (77.1%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,4 +1,17 @@
 class Solution {
+    public int minDifference(int[] nums) {
+        int n = nums.length;
+        int mini = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        if(n < 5) return 0;
+        for(int i = 0 ; i < 4 ; i++){
+            mini = Math.min(mini , nums[n - 4 + i] - nums[i]);
+        }
+        return mini;
+    }
+}
+
+/*class Solution {
     public int minDifference(int[] a) {
         int n = a.length;
         if(n <= 4) return 0;
@@ -9,4 +22,4 @@ class Solution {
         mini = Math.min(Math.min(a[n - 4] - a[0] , a[n - 3] - a[1]) , Math.min(a[n -2 ] - a[2] , a[n -1 ] - a[3]));
         return mini;
     }
-}
+} */
