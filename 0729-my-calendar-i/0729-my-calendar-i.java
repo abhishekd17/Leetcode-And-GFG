5219321bc6d1324b60af231735557949bb548570
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-02 03:20:37 +0530

    Time: 429 ms (5.06%), Space: 45.9 MB (14.06%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,20 +1,41 @@
 class MyCalendar {
-    private List<int[]> list;
+    Map<Integer , Integer> map;
+    public MyCalendar() {
+        map = new TreeMap<>();
+    }
+    
+    public boolean book(int start, int end) {
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        int sum = 0;
+        for(Map.Entry<Integer , Integer> entry : map.entrySet()){
+            sum += entry.getValue();
+            if(sum > 1){
+                map.put(start , map.get(start) - 1);
+                map.put(end , map.get(end) + 1);
+                return false;
+            }
+        }
+        return true;
+    }
+}
+/*class MyCalendar {
+    List<int[]> list;
     public MyCalendar() {
         list = new ArrayList<>();
     }
     
     public boolean book(int start, int end) {
-        for(int[] b : list){
-            if(Math.max(b[0] , start ) < Math.min(b[1] , end)) return false;
+        for(int[] l : list){
+            if(Math.max(start , l[0]) < Math.min(end , l[1])) return false;
         }
         list.add(new int[]{start , end});
         return true;
     }
-}
+}*/
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar obj = new MyCalendar();
- * boolean param_1 = obj.book(start,end);
- */
+ * boolean param_1 = obj.book(startTime,endTime);
+ */ 
