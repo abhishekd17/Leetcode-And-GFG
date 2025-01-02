9c9a5bf6f80115c2f597538e6216ea0d9bb1fd7a
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-02 15:43:36 +0530

    Time: 9 ms (35.76%), Space: 85.7 MB (76.77%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -4,28 +4,17 @@ class Solution {
         int q = queries.length;
         int ans[] = new int[q];
         int pref[] = new int[n + 1];
-        pref[0] = 0;
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
         for(int i = 0 ; i < n ; i++){
-            String s = words[i];
-            char first = s.charAt(0);
-            char last = s.charAt(s.length() - 1);
-            int cnt = 0;
-            if(check(first) && check(last)) cnt = 1;
-            pref[i + 1] = pref[i] + cnt;
+            pref[i + 1] = pref[i];
+            if (vowels.contains(words[i].charAt(0)) && vowels.contains(words[i].charAt(words[i].length() - 1))) {
+                pref[i + 1]++;
+            }
         }
-        int p = 0;
-        for(int[] query : queries){
-            int start = query[0];
-            int end = query[1];
-            String s = words[start];
-            char first = s.charAt(0);
-            char last = s.charAt(s.length() - 1);
-            ans[p++] = pref[end + 1] - pref[start + 1] + ((check(first) && check(last)) ? 1 : 0); 
+        for(int i = 0 ; i < q ; i++){
+            ans[i] = pref[queries[i][1] + 1] - pref[queries[i][0]]; 
         }
         return ans;
-    }
-    private boolean check(char c){
-        return c =='a' || c == 'e'|| c == 'i' || c == 'o' || c=='u';
     }
 }
 
