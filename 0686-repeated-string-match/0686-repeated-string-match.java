b545c579c55dd864b8c53683bd4ae140dbecd267
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-04 01:29:33 +0530

    Time: 271 ms (27.49%), Space: 45.4 MB (12.66%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -2,6 +2,20 @@ class Solution {
     public int repeatedStringMatch(String a, String b) {
         int cnt = 1;
         String s = a;
+        while (a.length() < b.length()) {
+            cnt++;
+            a += s;
+        }
+        if(a.contains(b)) return cnt;
+        a += s;
+        if(a.contains(b)) return ++cnt;
+        return -1;
+    }
+}
+/*class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int cnt = 1;
+        String s = a;
         while (!a.contains(b)) {
             if (a.length() >= Math.pow(10,4)) { 
                 return -1; 
@@ -12,4 +26,4 @@ class Solution {
 
         return cnt;
     }
-}
+} */
