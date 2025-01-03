d1f7f96b6744dbca8390e0c9e127e2220c11ea13
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-04 02:26:28 +0530

    Time: 259 ms (77.88%), Space: 42.5 MB (53.83%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,17 +1,36 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int cnt = 1;
-        String s = a;
-        while (a.length() < b.length()) {
-            cnt++;
-            a += s;
+        int n = b.length() / a.length();
+        int cnt = n;
+        StringBuilder sb = new StringBuilder();
+
+        while(cnt-- > 0){
+            sb.append(a);
         }
-        if(a.contains(b)) return cnt;
-        a += s;
-        if(a.contains(b)) return ++cnt;
+
+        if(sb.toString().contains(b)) return n;
+        sb.append(a);
+        if(sb.toString().contains(b)) return n + 1;
+        sb.append(a);
+        if(sb.toString().contains(b)) return n + 2;
         return -1;
     }
 }
+
+// class Solution {
+//     public int repeatedStringMatch(String a, String b) {
+//         int cnt = 1;
+//         String s = a;
+//         while (a.length() < b.length()) {
+//             cnt++;
+//             a += s;
+//         }
+//         if(a.contains(b)) return cnt;
+//         a += s;
+//         if(a.contains(b)) return ++cnt;
+//         return -1;
+//     }
+// }
 /*class Solution {
     public int repeatedStringMatch(String a, String b) {
         int cnt = 1;
