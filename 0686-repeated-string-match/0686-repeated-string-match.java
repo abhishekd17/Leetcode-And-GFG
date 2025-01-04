5c8f9e486368ac653d31d7263c18a541511f5b5e
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-05 01:03:58 +0530

    Time: 14 ms (85.04%), Space: 45.5 MB (7.84%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,18 +1,56 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int n = b.length() / a.length();
-        int cnt = n;
-        StringBuilder sb = new StringBuilder();
+        if(a.equals(b)) return 1;
+        int cnt = 1;
+        String ss= a;
+        while(ss.length() < b.length()){
+            cnt++;
+            ss += a;
+        }
+        if(ss.equals(b)) return cnt;
+        if(rabinKarp(ss , b) != -1) return cnt;
+        if(rabinKarp(ss + a , b) != -1) return cnt + 1;
+        return -1;
+    }
+    private static final int BASE = 1000000;
 
-        while(cnt-- > 0){
-            sb.append(a);
+    private static int rabinKarp(String s, String t) {
+        // first case
+        if (s.isEmpty() || t.isEmpty()) return -1;
+
+        int m = t.length();
+        int power = 1;
+
+        // compute power = (31^(m -1)) % base
+        for (int i = 0; i < m; i++) {
+            power = (int) ((long) power * 31) % BASE;
         }
 
-        if(sb.toString().contains(b)) return n;
-        sb.append(a);
-        if(sb.toString().contains(b)) return n + 1;
-        sb.append(a);
-        if(sb.toString().contains(b)) return n + 2;
+        int targetCode = 0;
+        int hashCode = 0;
+
+        // compute hash codes for the target and the initial window in the source
+        for (int i = 0; i < m; i++) {
+            targetCode = (int) (((long) targetCode * 31 + t.charAt(i))) % BASE;
+        }
+
+        // sliding window over the source string
+        for (int i = 0; i < s.length(); i++) {
+            hashCode = (int) ((long) hashCode * 31 + s.charAt(i)) % BASE;
+            if (i < m - 1) continue;
+            if (i >= m) {
+                // remove the influence of the outgoing char
+                hashCode = (int) ((long) hashCode - (long) s.charAt(i - m) * power) % BASE;
+            }
+            if (hashCode < 0) hashCode += BASE;
+
+            // now check if hashcode matches with target hash
+            if (hashCode == targetCode) {
+                if (s.substring(i - m + 1, i + 1).equals(t)) {
+                    return i - m + 1;
+                }
+            }
+        }
         return -1;
     }
 }
