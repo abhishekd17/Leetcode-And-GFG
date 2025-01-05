3048d85bea784089825b38cb5a9e53438fe8e036
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-05 23:52:02 +0530

    Time: 6 ms (5.22%), Space: 41.5 MB (41.8%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,5 +1,5 @@
 // brute force
-class Solution {
+/*class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length() != goal.length()) return false;
         for(int i=0;i<s.length();i++){
@@ -11,14 +11,50 @@ class Solution {
         return false;
     }
 }
-
+*/
 //optimal
-/*class Solution {
+class Solution {
     public boolean rotateString(String s, String goal) {
         int n=s.length();
         if(s.length() != goal.length()) return false;
-        String conc=s + s;
-        if(conc.contains(goal)) return true;
-        return false; 
+        // String conc=s + s;
+        // if(conc.contains(goal)) return true;
+        // return false; 
+        int[] z = calculateZ(goal + "$" + s + s);
+        for(int i =  0 ; i < z.length; i++){
+            if(z[i] == goal.length()) return true;
+        }
+        return false;
     }
-}*/
+    private static int[] calculateZ(String s){
+        int n = s.length();
+        int z[] = new int[n];
+        int left = 0;
+        int right = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(i > right){
+                left = right = i;
+                while(right < n && s.charAt(right) == s.charAt(right - left)){
+                    right++;
+                }
+                z[i] = right - left;
+                right--;
+            }else{
+                // we are operating inside the box
+                int k1 = i - left;
+                // if value does not stretches till right bound then just copy it
+                if(z[k1] < right - i + 1){
+                    z[i] = z[k1];
+                }else{ // try to see if there are more matches
+                    left  = i;
+                    while(right < n  && s.charAt(right) == s.charAt(right - left)){
+                        right++;
+                    }
+                    z[i] = right - left;
+                    right--;
+                }
+            }
+        }
+        return z;
+    }
+}
