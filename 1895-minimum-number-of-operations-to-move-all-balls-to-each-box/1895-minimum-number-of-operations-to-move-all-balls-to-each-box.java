7a6c2d7925b424c9e2c58c977e9d30da6457deb2
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-06 17:43:36 +0530

    Time: 4 ms (76.56%), Space: 45 MB (32.47%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,4 +1,32 @@
 class Solution {
+    public int[] minOperations(String s) {
+        int n = s.length();
+
+        int pref[] = new int[n + 1];
+        int suff[] = new int[n + 1];
+
+        for(int i = 0 ; i < n ; i++){
+            pref[i + 1] = pref[i] + (s.charAt(i) == '1' ? 1 : 0);
+            suff[n - i - 1] = suff[n - i] + (s.charAt(n - i - 1) == '1' ? 1 : 0);
+        }
+
+        for(int i = 1 ; i < n ; i++ ){
+            pref[i] += pref[i - 1];
+            suff[n - i - 1] += suff[n - i];
+        }
+        // for(int i = 0 ; i < n + 1 ; i++){
+        //     System.out.println(pref[i] + " "+ suff[i]);
+        // }
+
+        int ans[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            ans[i] = pref[i] + suff[i + 1];
+        }
+        return ans;
+    }
+}
+
+/*class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
         int ans[] = new int[n];
@@ -14,4 +42,4 @@ class Solution {
         }
         return ans;
     }
-}
+} */
