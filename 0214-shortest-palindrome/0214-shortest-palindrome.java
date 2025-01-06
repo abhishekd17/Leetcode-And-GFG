eed813faf5c5da41c2a15222285189e86b8a01a3
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-06 22:22:20 +0530

    Time: 8 ms (73.79%), Space: 45.8 MB (19.02%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,17 +1,44 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        int i = 0, end = s.length() - 1, j = end;
-        char chs[] = s.toCharArray();
-        while (i < j) {
-            if (chs[i] == chs[j]) {
+        // int i = 0, end = s.length() - 1, j = end;
+        // char chs[] = s.toCharArray();
+        // while (i < j) {
+        //     if (chs[i] == chs[j]) {
+        //         i++;
+        //         j--;
+        //     } else {
+        //         i = 0;
+        //         end--;
+        //         j = end;
+        //     }
+        // }
+        // return new StringBuilder(s.substring(end + 1)).reverse().toString() + s;
+
+        String temp = s + "#" + new StringBuilder(s).reverse().toString();
+        int z[] = computeLPS(temp);
+        return new StringBuilder(s.substring(z[z.length - 1])).reverse().toString() + s;
+    }
+
+    public static int[] computeLPS(String pattern){
+        int m = pattern.length();
+        int lps[] = new int[m];
+        int len = 0;
+        int i = 1 ;
+
+        while(i  < m ){
+            if(pattern.charAt(i) == pattern.charAt(len)){
+                len++;
+                lps[i] = len;
                 i++;
-                j--;
-            } else {
-                i = 0;
-                end--;
-                j = end;
+            }else{
+                if(len != 0){
+                    len = lps[len - 1];
+                }else{
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return new StringBuilder(s.substring(end + 1)).reverse().toString() + s;
+        return lps;
     }
 }
