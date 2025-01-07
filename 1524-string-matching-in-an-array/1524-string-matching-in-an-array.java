4514d5a85426b5ac3c003fc4172968fca071b9f8
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-01-07 12:59:26 +0530

    Time: 16 ms (6.83%), Space: 45.1 MB (6%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -2,10 +2,12 @@ class Solution {
     public List<String> stringMatching(String[] words) {
         List<String> ans = new ArrayList<>();
         int n = words.length;
+        Arrays.sort(words, Collections.reverseOrder());
+        // System.out.println(words[0]);
         for(int i = 0 ; i < n ; i++){
             String s = words[i];
             for(int j = 0 ; j < n ; j++){
-                if(!s.equals(words[j]) && s.contains(words[j])){
+                if(i != j && KMP(words[i] , words[j])){
                     if(!ans.contains(words[j])){
                         ans.add(words[j]);
                     }
@@ -13,5 +15,53 @@ class Solution {
             }
         }
         return ans;
+    }
+    public static int[] computeLPS(String pattern){
+        int m = pattern.length();
+        int lps[] = new int[m];
+        int len = 0;
+        int i = 1 ;
+
+        while(i  < m ){
+            if(pattern.charAt(i) == pattern.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len != 0){
+                    len = lps[len - 1];
+                }else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    private static boolean KMP(String text , String pattern){
+        int n = text.length();
+        int m = pattern.length();
+        if(pattern.isEmpty()) return true;
+        int lps[] = computeLPS(pattern);
+
+        int i = 0 , j  = 0;
+
+        while(i < n){
+            if(text.charAt(i) == pattern.charAt(j)){
+                i++;
+                j++;
+            }
+            if(j == m){
+                return true;
+            }else if(i < n  && text.charAt(i) != pattern.charAt(j)){
+                if(j != 0){
+                    j = lps[j - 1];
+                }else{
+                    i++;
+                }
+            }
+        }
+        return false;
     }
 }
