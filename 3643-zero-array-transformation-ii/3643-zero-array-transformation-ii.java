b91abd88b6ea001403ae131a259900db15984741
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-14 03:31:44 +0530

    Time: 25 ms (42.09%), Space: 127.4 MB (27.68%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -1,31 +1,96 @@
 class Solution {
-    public int minZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
-        
-        if (Arrays.stream(nums).allMatch(x -> x == 0)) return 0;
-        int left = 1, right = queries.length;
-        if (!canMakeZeroArray(right, nums, queries)) return -1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canMakeZeroArray(mid, nums, queries)) right = mid;
-            else left = mid + 1;
-        }
-        return left;
-    }
 
-    private boolean canMakeZeroArray(int k, int[] nums, int[][] queries) {
-        int n = nums.length;
-        int[] diff = new int[n + 1];
-        for (int i = 0; i < k; i++) {
-            int left = queries[i][0], right = queries[i][1], val = queries[i][2];
-            diff[left] += val;
-            diff[right + 1] -= val;
+    private boolean helper(int nums[] , int[][] queries , int k){
+        int a[] = new int[nums.length];
+
+        for(int i = 0 ; i <= k ; i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int x = queries[i][2];
+
+            a[l] += x;
+            if(r + 1 < nums.length ) a[r + 1] -= x; 
         }
-        int currVal = 0;
-        for (int i = 0; i < n; i++) {
-            currVal += diff[i];
-            if (currVal < nums[i]) return false;
+
+        int cumSum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            cumSum += a[i];
+            a[i] = cumSum;
+
+            if(nums[i] > a[i]) return false;
         }
         return true;
     }
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int q = queries.length;
+
+        boolean flag = true;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] != 0){
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag) return 0;
+        int l = 0 , r = q - 1 , ans = -1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+
+            if(helper(nums , queries , mid) == true){
+                r = mid - 1;
+                ans = mid + 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
+
+/*class Solution {
+
+    private boolean helper(int nums[] , int[][] queries , int k){
+        int a[] = new int[nums.length];
+
+        for(int i = 0 ; i <= k ; i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int x = queries[i][2];
+
+            a[l] += x;
+            if(r + 1 < nums.length ) a[r + 1] -= x; 
+        }
+
+        int cumSum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            cumSum += a[i];
+            a[i] = cumSum;
+
+            if(nums[i] > a[i]) return false;
+        }
+        return true;
+    }
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int q = queries.length;
+
+        boolean flag = true;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] != 0){
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag) return 0;
+
+        for(int i = 0 ; i < q ; i++){
+            if(helper(nums, queries , i) == true) return i + 1;
+        }
+        return -1;
+    }
+} */
