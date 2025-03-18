0d4a6cf0ce2626b65028de8c53cedbc6f3b078bf
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-19 02:28:39 +0530

    Time: 7 ms (16.45%), Space: 55.4 MB (85.94%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,12 +1,27 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-    int used = 0, j = 0, res = 0;
-    for (int i = 0; i < nums.length; ++i) {
-        while ((used & nums[i]) != 0)
-            used ^= nums[j++];
-        used |= nums[i];
-        res = Math.max(res, i - j + 1);
+        int n = nums.length;
+
+        int ans = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
+                if(isNice(nums , i , j)){
+                    ans = Math.max(ans , j - i + 1);
+                }else{
+                    break;
+                }
+            }
+        }
+        return ans;
     }
-    return res;    
-}
+
+    private boolean isNice(int nums[] , int i , int j){
+        int mask = 0;
+        for(int k = i ; k <= j ; k++){
+            if((mask & nums[k]) != 0) return false;
+            mask = mask | nums[k];
+        }
+        return true;
+    }
 }
