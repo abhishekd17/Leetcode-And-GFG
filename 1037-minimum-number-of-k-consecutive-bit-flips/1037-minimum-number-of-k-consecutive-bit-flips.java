68e87a4eeab935c2a2afcfc5fa85fc500fb5a9e4
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-21 20:36:45 +0530

    Time: 5 ms (80.33%), Space: 56.1 MB (60.94%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
@@ -1,29 +1,39 @@
 class Solution {
     public int minKBitFlips(int[] nums, int k) {
         int n = nums.length;
-        int ans = 0;
-        int[] flip = new int[n + 1]; // To track the flips
-        int currentFlips = 0; // Current number of flips affecting the current position
+        int[] flip = new int[n]; // To track flips
+        int f = 0; // Current number of flips
+        int ans = 0; // Count of flips
 
-        for (int i = 0; i < n; i++) {
-            currentFlips ^= flip[i]; // Update current flips based on previous flips
+        for (int i = 0; i <= n - k; i++) {
+            // Update the current flip count based on previous flips
+            if (i - k >= 0) {
+                f -= flip[i - k]; // Remove the effect of the flip that ends at i - k
+            }
 
-            // If the current bit is 0 after considering flips, we need to flip
-            if ((nums[i] ^ currentFlips) == 0) {
-                if (i + k > n) {
-                    return -1; // Not enough bits to flip
-                }
-                ans++; // Increment the flip count
-                currentFlips ^= 1; // We are flipping the current bit
-                flip[i] ^= 1; // Mark the start of a flip
-                if (i + k < n) {
-                    flip[i + k] ^= 1; // Mark the end of a flip
-                }
+            // Determine the effective value of nums[i] after considering flips
+            if ((nums[i] + f) % 2 == 0) { // If the effective value is 0
+                ans++; // We need to flip
+                f++; // Increment the flip count
+                flip[i] = 1; // Mark the start of a flip
             }
         }
-        return ans;
+
+        // Check the last k elements
+        for (int i = n - k + 1; i < n; i++) {
+            if (i - k >= 0) {
+                f -= flip[i - k]; // Remove the effect of the flip that ends at i - k
+            }
+            // If the effective value is 0, we cannot flip
+            if ((nums[i] + f) % 2 == 0) {
+                return -1; // Not enough bits to flip
+            }
+        }
+
+        return ans; // Return the total number of flips
     }
 }
+
 // brute force approch
 /*class Solution {
     public int minKBitFlips(int[] nums, int p) {
@@ -33,7 +43,7 @@ class Solution {
         for(int i = 0 ; i <= n - p ; i++){
             if(nums[i] == 0){
                 for(int j = i ; j < i + p ; j++){
-
+                    
                     nums[j] ^= 1;
                 }
                 ans++;
