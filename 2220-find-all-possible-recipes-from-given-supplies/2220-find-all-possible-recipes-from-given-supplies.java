fc18c3f4d5c8cfabdf85d29f5efe83ebbd8c20f5
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-24 01:39:24 +0530

    Time: 33 ms (77.62%), Space: 46 MB (98.99%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -1,4 +1,53 @@
 class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        int n = recipes.length;
+        // add ing. in set
+        Set<String> set = new HashSet<>(Arrays.asList(supplies));
+
+        Map<String , List<Integer>> adj = new HashMap<>();
+        int indegree[] = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            for(String ing : ingredients.get(i)){
+                if(!set.contains(ing)){
+                    adj.putIfAbsent(ing , new ArrayList<>());
+                    adj.get(ing).add(i);
+                    indegree[i]++;
+                }
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int i = q.poll();
+            String recipe = recipes[i];
+            ans.add(recipe);
+
+            if(adj.containsKey(recipe)){
+                for(int idx : adj.get(recipe)){
+                    indegree[idx]--;
+                    if(indegree[idx] == 0){
+                        q.add(idx);
+                    }
+                }
+            }
+        }
+        return ans;        
+    }
+}
+
+
+// brute force
+/*class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> list, String[] supplies) {
         int n = recipes.length;
 
@@ -29,4 +78,4 @@ class Solution {
         }
         return ans;
     }
-}
+} */
