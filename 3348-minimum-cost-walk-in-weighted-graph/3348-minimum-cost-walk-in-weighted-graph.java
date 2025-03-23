8d02df779ef40247a461c4e59a83e0e7cbef47c1
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-23 21:06:02 +0530

    Time: 8 ms (49.92%), Space: 104.8 MB (98.38%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.java
@@ -1,44 +1,94 @@
 class Solution {
-    private int findRoot(int[] parent, int node) {
-        if (parent[node] != node) {
-            parent[node] = findRoot(parent, parent[node]);
+    public int[] minimumCost(int n, int[][] edges, int[][] query) {
+
+        DSU dsu = new DSU(n);
+        int nn = query.length;
+
+        int cost[] = new int[n];
+        int ans[] = new int[nn];
+
+        Arrays.fill(cost , -1);
+
+        for(int[] edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+    
+            if(dsu.findParent(u) != dsu.findParent(v)){
+                int pU = dsu.findParent(u);
+                int pV = dsu.findParent(v);
+                dsu.unionBySize(u , v);
+                int newParent = dsu.findParent(u);
+                cost[newParent] = cost[pU] & cost[pV] & w;
+            }else{
+                cost[dsu.findParent(u)] &= w;
+            }
+            cost[dsu.findParent(u)] &= cost[v] & w; 
         }
-        return parent[node];
+        int i = 0;
+        for(int[] q : query){
+            int u = q[0];
+            int v = q[1];
+            if(u == v) ans[i++] = 0;
+            else if(dsu.findParent(u) != dsu.findParent(v)) ans[i++] = -1;
+            else{
+                ans[i++] = cost[dsu.findParent(u)];
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+class DSU{
+    int parent[] ;
+    int rank[];
+    int size[];
+
+    public DSU(int n){
+        parent = new int[n];
+        rank = new int[n];
+        size = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i ;
+            rank[i] = 0;
+            size[i] = 1;
+        }
     }
 
-    public int[] minimumCost(int n, int[][] edges, int[][] query) {
-        int[] parent = new int[n];
-        int[] minPathCost = new int[n];
-        Arrays.fill(minPathCost, -1);
+    int findParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
 
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+    void unionByRank(int u , int v){
+        int pU = findParent(u);
+        int pV = findParent(v);
+
+        if(pU == pV) return;
+
+        if(rank[pU] > rank[pV]){
+            parent[pV] = pU;
+        }else if(rank[pU] < rank[pV]){
+            parent[pU] = pV;
+        }else{
+            parent[pU] = pV;
+            rank[pV]++;
         }
+    }
 
-        for (int[] edge : edges) {
-            int source = edge[0], target = edge[1], weight = edge[2];
-            int sourceRoot = findRoot(parent, source);
-            int targetRoot = findRoot(parent, target);
+    void unionBySize(int u , int v){
+        int pU = findParent(u);
+        int pV = findParent(v);
 
-            minPathCost[targetRoot] &= weight;
-
-            if (sourceRoot != targetRoot) {
-                minPathCost[targetRoot] &= minPathCost[sourceRoot];
-                parent[sourceRoot] = targetRoot;
-            }
+        if(pU == pV) return;
+        if(size[pU] > size[pV]){
+            parent[pV] = pU;
+            size[pU] += size[pV];
+        }else{
+            parent[pU] = pV;
+            size[pV] += size[pU];
         }
-
-        int[] result = new int[query.length];
-        for (int i = 0; i < query.length; i++) {
-            int start = query[i][0], end = query[i][1];
-            if (start == end) {
-                result[i] = 0;
-            } else if (findRoot(parent, start) != findRoot(parent, end)) {
-                result[i] = -1;
-            } else {
-                result[i] = minPathCost[findRoot(parent, start)];
-            }
-        }
-        return result;
     }
 }
