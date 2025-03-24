a1ff3938d6c87855cda0c40777af2eb76fe30a4d
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-24 20:30:22 +0530

    Time: 12 ms (56.01%), Space: 45.1 MB (85.02%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -1,53 +1,107 @@
-public class DSU {
-    private int[] p, rank, count;
+class Solution {
+    public int countCompleteComponents(int n, int[][] edges) {
 
-    public DSU(int n) {
-        p = new int[n];
-        rank = new int[n];
-        count = new int[n];
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            list.add(new ArrayList());
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+
+        int vis[] = new int[n];
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-            p[i] = i;
-            count[i] = 1;
+            if (vis[i] == 0) {
+                List<Integer> components = new ArrayList<>();
+                dfs(i, vis, components , list);
+
+                if (isCompleteComponent(components, list)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public void dfs(int node, int vis[], List<Integer> components, List<List<Integer>> adj) {
+        vis[node] = 1;
+        components.add(node);
+        for (int neighbor : adj.get(node)) {
+            if (vis[neighbor] == 0) {
+                dfs(neighbor, vis, components, adj);
+            }
         }
     }
 
-    public int find(int x) {
-        if (x != p[x])
-            p[x] = find(p[x]);
-        return p[x];
-    }
-
-    public void unionSet(int x, int y) {
-        int xx = find(x), yy = find(y);
-        if (xx == yy) return;
-        count[xx] = count[yy] = count[xx] + count[yy];
-        if (rank[xx] < rank[yy]) p[xx] = yy;
-        else p[yy] = xx;
-        if (rank[xx] == rank[yy]) rank[xx]++;
-    }
-
-    public int sizeOfGroupThatXIsAPartOf(int x) {
-        return count[find(x)];
+    private boolean isCompleteComponent(List<Integer> components, List<List<Integer>> adj) {
+        int k = components.size();
+        if (k == 1)
+            return true;
+        int requiredEdges = k * (k - 1) / 2;
+        int actualEdges = 0;
+        for (int node : components) {
+            actualEdges += adj.get(node).size();
+        }
+        actualEdges /= 2;
+        return requiredEdges == actualEdges;
     }
 }
 
-class Solution {
-    public int countCompleteComponents(int n, int[][] edges) {
-        DSU uf = new DSU(n);
-        Set<Integer> groups = new HashSet<>();
-        int[] counter = new int[n];
-        for (int[] e : edges) {
-            uf.unionSet(e[0], e[1]);
-            counter[e[0]]++; counter[e[1]]++;
+class DSU {
+    int parent[];
+    int rank[];
+    int size[];
+
+    public DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+            rank[i] = 0;
         }
+    }
 
-        for (int i = 0; i < n; i++)
-            groups.add(uf.find(i));
+    public int findParent(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
 
-        for (int i = 0; i < n; i++)
-            if (uf.sizeOfGroupThatXIsAPartOf(i) != counter[i] + 1)
-                groups.remove(uf.find(i));
+    public void unionByRank(int u, int v) {
+        int pU = findParent(u);
+        int pV = findParent(v);
 
-        return groups.size();
+        if (pU == pV)
+            return;
+
+        if (rank[pU] > rank[pV]) {
+            parent[pV] = pU;
+        } else if (rank[pU] < rank[pV]) {
+            parent[pU] = pV;
+        } else {
+            parent[pU] = pV;
+            rank[pV]++;
+        }
+    }
+
+    private void unionBySize(int u, int v) {
+        int pU = findParent(u);
+        int pV = findParent(v);
+
+        if (pU == pV)
+            return;
+        if (size[pU] > size[pV]) {
+            parent[pV] = pU;
+            size[pU] += size[pV];
+        } else {
+            parent[pU] = pV;
+            size[pV] += size[pU];
+        }
     }
 }
