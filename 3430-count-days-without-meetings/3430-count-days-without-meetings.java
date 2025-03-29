db5756cd86a9924922f52381931de7ee23dd2681
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-29 21:13:20 +0530

    Time: 38 ms (76.68%), Space: 101 MB (12.92%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,27 +1,42 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+        int n = meetings.length;
+        Arrays.sort(meetings , (a,b) -> Integer.compare(a[0] , b[0]));
 
-        int meetingDaysCount = 0;
-        int currentStart = -1, currentEnd = -1;
+        int ans = meetings[0][0] - 1;
+        int end = meetings[0][1];
 
-        for (int[] meeting : meetings) {
-            int start = meeting[0], end = meeting[1];
-            if (start > currentEnd) {
-                if (currentEnd != -1) {
-                    meetingDaysCount += (currentEnd - currentStart + 1);
-                }
-                currentStart = start;
-                currentEnd = end;
-            } else {
-                currentEnd = Math.max(currentEnd, end);
+        for(int i = 1 ; i < n ; i++){
+            if(meetings[i][0] > end){
+                ans += (meetings[i][0] - end - 1);
             }
+            end = Math.max(end , meetings[i][1]);
         }
 
-        if (currentEnd != -1) {
-            meetingDaysCount += (currentEnd - currentStart + 1);
-        }
-
-        return days - meetingDaysCount;
+        ans += (days - end);
+        return ans;
     }
 }
+
+
+/*class Solution {
+    public int countDays(int days, int[][] meetings) {
+        int vis[] = new int[days + 1];
+        vis[0] = 1;
+
+        for(int[] meeting : meetings){
+            int start = meeting[0];
+            int end = meeting[1];
+            for(int i = start ; i <= end ; i++){
+                vis[i] = 1;
+            } 
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i <= days ; i++){
+            if(vis[i] == 0) ans++;
+        }
+
+        return ans;
+    }
+} */
