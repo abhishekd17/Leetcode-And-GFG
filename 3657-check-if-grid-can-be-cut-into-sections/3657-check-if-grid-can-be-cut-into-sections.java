e6ee090ea8cc731438f65e89382a66e5a8ee6e02
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-29 22:21:27 +0530

    Time: 843 ms (5%), Space: 129 MB (10.22%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.java
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.java
@@ -1,31 +1,34 @@
 class Solution {
-    public boolean checkValidCuts(int n, int[][] rectangles) {
-        int[][] xIntervals = new int[rectangles.length][2];
-        int[][] yIntervals = new int[rectangles.length][2];
+    public boolean checkValidCuts(int n, int[][] nums) {
+        int r = nums.length;
+        int xAxis[][] = new int[r][2];
+        int yAxis[][] = new int[r][2];
 
-        for (int i = 0; i < rectangles.length; i++) {
-            xIntervals[i][0] = rectangles[i][0];
-            xIntervals[i][1] = rectangles[i][2];
-            yIntervals[i][0] = rectangles[i][1];
-            yIntervals[i][1] = rectangles[i][3];
+        for(int i = 0 ; i < r ; i++){
+            xAxis[i][0] = nums[i][0];
+            xAxis[i][1] = nums[i][2];
+            yAxis[i][0] = nums[i][1];
+            yAxis[i][1] = nums[i][3];
         }
 
-        return check(xIntervals) || check(yIntervals);
-    }
+        Arrays.sort(xAxis, (a , b) -> Integer.compare(a[0] , b[0]));
+        Arrays.sort(yAxis, (a , b) -> Integer.compare(a[0] , b[0]));
 
-    private boolean check(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int sections = 0;
-        int maxEnd = intervals[0][1];
+        int x = 0 , y = 0;
+        int endX = xAxis[0][1] , endY = yAxis[0][1];
 
-        for (int[] interval : intervals) {
-            if (maxEnd <= interval[0]) {
-                sections++;
+        for(int i = 1 ; i < r ; i++){
+            if(xAxis[i][0] >= endX){
+                x++;
             }
-            maxEnd = Math.max(maxEnd, interval[1]);
+            endX = Math.max(endX , xAxis[i][1]);
+            if(yAxis[i][0] >= endY){
+                y++;
+            }
+            endY = Math.max(endY , yAxis[i][1]);
+            System.out.println(x + " " + y);
         }
 
-        return sections >= 2;
+        return (x >= 2 || y >= 2);
     }
 }
