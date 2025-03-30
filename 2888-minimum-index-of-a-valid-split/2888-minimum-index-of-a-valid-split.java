69b8e3512d90f770b926f88d42cd5e5e45ccc301
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-30 22:39:14 +0530

    Time: 60 ms (5.3%), Space: 58.1 MB (43.12%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,22 +1,18 @@
 class Solution {
-  public int minimumIndex(List<Integer> nums) {
-    var map = new HashMap<Integer, Integer>();
-    map.put(0, 0);
-    int max = 0, m = nums.size();
-    
-    for (var n : nums) {
-      map.put(n, map.getOrDefault(n, 0) + 1);
-      
-      if (map.get(n) > map.get(max))
-        max = n;
+    public int minimumIndex(List<Integer> nums) {
+        int n = nums.size();
+        Map<Integer , Integer> map1 = new HashMap<>();
+        Map<Integer , Integer> map2 = new HashMap<>();
+
+        for(int x : nums){
+            map2.put(x , map2.getOrDefault(x , 0) + 1);
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            map1.put(nums.get(i) , map1.getOrDefault(nums.get(i) , 0) + 1);
+            map2.put(nums.get(i) , map2.getOrDefault(nums.get(i) , 0) - 1);
+            if(map1.get(nums.get(i)) > (i + 1) / 2 && map2.get(nums.get(i)) > (n - i -1) / 2) return i;
+        }
+        return -1;
     }
-    var freq = 0;
-    for (var i=0; i<m; i++) {
-      if (nums.get(i) == max) freq++;
-      
-      if (freq * 2 > i+1 && (map.get(max) - freq) * 2 > (m-i-1))
-        return i;
-    }
-    return -1;
-  }
 }
