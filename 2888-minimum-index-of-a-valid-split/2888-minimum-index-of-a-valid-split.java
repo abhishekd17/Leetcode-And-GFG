c53088d3352025eee0f2632f4c047704c6be965c
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-03-30 22:51:01 +0530

    Time: 9 ms (98.8%), Space: 55.5 MB (96.8%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,6 +1,39 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
         int n = nums.size();
+        int el = nums.get(0);
+        int cnt = 1;
+
+        for(int i = 1 ; i < n ;i++){
+            if(cnt == 0){
+                el = nums.get(i);
+                cnt = 1;
+            }else if(nums.get(i) == el){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        int total = 0;
+        for(int i = 0 ; i < n ;i++){
+            if(nums.get(i) == el) total++;
+        }
+        int x = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums.get(i) == el){
+                x++;
+                total--;
+            }
+            if(x > (i + 1) / 2  && total > (n - i - 1) / 2) return i;
+        }
+        return -1;
+    }
+}
+
+
+/*class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        int n = nums.size();
         Map<Integer , Integer> map1 = new HashMap<>();
         Map<Integer , Integer> map2 = new HashMap<>();
 
@@ -15,4 +48,4 @@ class Solution {
         }
         return -1;
     }
-}
+} */
