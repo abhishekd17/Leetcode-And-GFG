ab617470985955d1552ca53d6288da30ca2c1168
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-04-04 02:16:02 +0530

    Time: 52 ms (98.41%), Space: 45.7 MB (93.94%) - LeetHub

--- 3805-maximize-active-section-with-trade-i/3805-maximize-active-section-with-trade-i.java
+++ 3805-maximize-active-section-with-trade-i/3805-maximize-active-section-with-trade-i.java
@@ -1,9 +1,44 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
+        int n = s.length() , i = 0 , one = 0 , zero = 0 ;
+        int curr = 0 , prev = 0;
+
+        while(i < n && s.charAt(i) == '1') {
+            one++; 
+            i++;
+        }
+        while(i < n && s.charAt(i) == '0') {
+            prev++;
+            i++;
+        }
+        while(i < n){
+            while(i < n && s.charAt(i) == '1') {
+                one++; 
+                i++;
+            }
+            if(i == n) break;
+
+            while(i < n && s.charAt(i) == '0') {
+                curr++;
+                i++;
+            }
+
+            zero = Math.max(zero , curr + prev);
+            prev = curr;
+            curr = 0;
+        }
+        return one + zero;
+    }
+}
+
+/*class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
         int n = s.length();
 
         List<int[]> list = new ArrayList<>(); 
+       
         int ones = 0;
+       
         for(int i = 0 ; i < n ;){
             if(s.charAt(i) == '0'){
                 int start = i;
@@ -16,8 +51,11 @@ class Solution {
                 i++;
             }
         }
+        
         if(list.size() < 2) return ones;
+        
         int maxi = 0;
+
         for(int i = 0 ; i < list.size() - 1; i++){
             boolean l = list.get(i)[0] + 1 < n ? s.charAt(list.get(i)[0] + list.get(i)[1]) == '1' : true;
             boolean r = list.get(i + 1)[0] - 1 >= 0 ? s.charAt(list.get(i + 1)[0] - 1) == '1' : true;
@@ -25,4 +63,4 @@ class Solution {
         }
         return maxi + ones;
     }
-}
+} */
