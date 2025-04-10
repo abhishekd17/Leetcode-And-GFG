4cc28eca906a3a8f964e42bc3259da0b6311718c
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-04-10 20:35:09 +0530

    Time: 111 ms (26.17%), Space: 113.6 MB (80.87%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,42 +1,52 @@
 class Solution {
-    private int timer  = 1;
-    private void dfs(int node,  int parent , int InsertionTime[] , int LowestTime[] , int vis[] ,List<List<Integer>> bridges , List<List<Integer>> adj){
-        vis[node] = 1;
-        InsertionTime[node] = LowestTime[node] = timer;
-        timer++;
+    private int time = 0;
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> list) {
+        List<List<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0 ; i < n ; i++) adj.add(new ArrayList<>());
+        
+        for(List<Integer> l : list){
+            adj.get(l.get(0)).add(l.get(1));
+            adj.get(l.get(1)).add(l.get(0));
+        }
 
-        for(int adjNode : adj.get(node)){
-            if(adjNode == parent) continue;
-            if(vis[adjNode] == 0){
-                dfs(adjNode , node , InsertionTime , LowestTime , vis , bridges , adj);
-                LowestTime[node] = Math.min(LowestTime[adjNode] , LowestTime[node]);
-                if(LowestTime[adjNode] > InsertionTime[node]){
-                    bridges.add(Arrays.asList(adjNode , node));
+        int disc[] = new int[n];
+        int low[] = new int[n];
+        int parent[] = new int[n];
+        
+        Arrays.fill(disc , -1);
+        Arrays.fill(low , -1);
+        Arrays.fill(parent , -1);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i++){
+            if(disc[i] == -1){
+                dfs(i , adj , disc , low , parent , ans);
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(int u , List<List<Integer>> adj , int disc[] , int low[] , int parent[] ,List<List<Integer>> ans){
+        disc[u] = low[u] = ++time;
+
+        for(int v : adj.get(u)){
+            if(disc[v] == -1){
+                parent[v] = u;
+                dfs(v, adj, disc, low, parent, ans);
+                low[u] = Math.min(low[u] , low[v]);
+
+                if(low[v] > disc[u]){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(u);
+                    temp.add(v);
+                    ans.add(temp);
                 }
             }
-            else{
-                LowestTime[node] = Math.min(LowestTime[adjNode] , LowestTime[node]); 
+            else if(v != parent[u]){
+                low[u] = Math.min(low[u] , disc[v]);
             }
         }
-    }
-    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        List<List<Integer>> adj = new ArrayList<>();
-        List<List<Integer>> bridges = new ArrayList<>();
-        for(int i = 0 ; i  < n ; i++){
-            adj.add(new ArrayList<>());
-        }
-
-        for(List<Integer> it : connections){
-            int u = it.get(0);
-            int v = it.get(1);
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-
-        int InsertionTime[] = new int[n];
-        int LowestTime[] = new int[n];
-        int vis[] = new int[n];
-        dfs(0 , -1 , InsertionTime , LowestTime , vis , bridges , adj);
-        return bridges;
     }
 }
