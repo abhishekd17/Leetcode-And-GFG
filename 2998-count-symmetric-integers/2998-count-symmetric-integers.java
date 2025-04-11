b18af8968e8c78b1a6d267b0b54c5d981674d7c0
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-04-11 23:46:06 +0530

    Time: 10 ms (98.56%), Space: 41.7 MB (86.87%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,6 +1,22 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
         int ans = 0;
+        for(int i = low ; i <= high ; i++){
+            if(i < 100 && i % 11 == 0) ans++;
+            else if(i >= 1000 && i <= 10000){
+                int left = i / 1000 + (i % 1000) / 100;
+                int right = (i % 100) / 10 + i % 10;
+                if(left == right) ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+
+/*class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int ans = 0;
 
         for(int i = low ; i <= high ; i++){
             String s = Integer.toString(i);
@@ -18,4 +34,4 @@ class Solution {
         }
         return ans;
     }
-}
+} */
