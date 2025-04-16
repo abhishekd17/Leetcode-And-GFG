64a6722899ec7f601dca923fcea028beaf35b3bf
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-04-17 00:10:49 +0530

    Time: 0 ms (100%), Space: 40.8 MB (29.71%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -1,16 +1,56 @@
 class Solution {
-    private long MOD=1_000_000_007;
+    private int mod = (int) 1e9 + 7;
     public int countGoodNumbers(long n) {
-        long even=(n+1)/2;
-        long odd=n/2;
-        long ans=helper(5,even) * helper( 4 , odd);
-        return (int)(ans % MOD);
+        long even = (n + 1) / 2;
+        long odd = n / 2;
+
+        long ans = pow(5 , even) % mod * pow(4 , odd) % mod;
+        return (int)ans;
     }
-    public long helper(long x , long n){
-        if(n==0) return 1;
-        long ans=helper(x,n/2);
-        if(n%2==0) return (ans*ans)%MOD;
-        else return (x*ans*ans)%MOD;
-        
+    private long pow(long base , long exp){
+        long ans = 1;
+        while(exp > 0){
+            if(exp % 2 == 1){
+                ans = (ans * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp /= 2;
+        }
+        return ans;
     }
 }
+
+
+/*class Solution {
+    private int mod = (int)1e9 + 7;
+    public int countGoodNumbers(long n) {
+        int cnt = 0;
+        long nn = (long) Math.pow(10 , n) - 1;
+        for(long i = 1 ; i <= nn ; i++){
+            String s = String.valueOf(i);
+            if(evenInd(s) && primeOdd(s)) cnt = (cnt + 1) % mod;
+        }
+        return cnt % mod;
+    }
+
+    private boolean evenInd(String s){
+        boolean isEvenInd = true;
+        for(int i = 0 ; i < s.length() ; i += 2){
+            if((s.charAt(i) - '0') % 2 != 0) return false;
+        }
+        return true;
+    }
+
+    private boolean primeOdd(String s){
+        boolean prime = true;
+        for(int i = 1 ; i < s.length() ; i += 2){
+            if(!isPrime(s.charAt(i) - '0')) return false;
+        }
+        return true;
+    }
+
+    private boolean isPrime(int n){
+        if(n == 2 || n == 3 || n == 5 || n == 7) return true;
+        return false;
+    }
+} */
