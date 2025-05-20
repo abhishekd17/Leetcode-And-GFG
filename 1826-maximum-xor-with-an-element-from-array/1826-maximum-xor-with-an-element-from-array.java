40639343e42fcd39768ea6fdfee2c86db7bd46c5
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-05-21 00:19:28 +0530

    Time: 148 ms (46.76%), Space: 139.1 MB (31.94%) - LeetHub

--- 1826-maximum-xor-with-an-element-from-array/1826-maximum-xor-with-an-element-from-array.java
+++ 1826-maximum-xor-with-an-element-from-array/1826-maximum-xor-with-an-element-from-array.java
@@ -1,5 +1,104 @@
-import java.util.*;
-class Node{
+class TrieNode{
+    TrieNode links[];
+    
+    public TrieNode(){
+       links = new TrieNode[2];
+    }
+
+    public boolean containsKey(int ind){
+        return links[ind] != null;
+    }
+
+    TrieNode get(int ind){
+        return links[ind];
+    }
+
+    void put(int ind , TrieNode node){
+        links[ind] = node;
+    }
+}
+
+class Trie{
+    TrieNode root;
+    Trie(){
+        root = new TrieNode();
+    }
+
+    void insert(int num){
+        TrieNode node = root;
+        for(int i = 31 ; i >= 0 ; i--){
+            int bit = (num >> i ) & 1 ;
+            if(!node.containsKey(bit)){
+                node.put(bit , new TrieNode());
+            }
+            node = node.get(bit);
+        }
+    }
+
+    int findMax(int num){
+        TrieNode node = root;
+        int maxNum = 0;
+        for(int i = 31 ; i >= 0 ; i--){
+            int bit = (num >> i) & 1;
+            int opp = 1 - bit;
+            if(node.links[opp] != null){
+                maxNum |= (1 << i);
+                node = node.get(opp);
+            }
+            else{
+                node = node.get(bit);
+            }
+        }
+        return maxNum;  
+    }
+}
+class Solution {
+    public int[] maximizeXor(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int q = queries.length;
+
+        int ans[] = new int[q];
+        ArrayList<query> qq = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0 ; i < q ; i++){
+            qq.add(new query(queries[i][0] , queries[i][1] , i));
+        }
+
+        Collections.sort(qq , Comparator.comparingInt(p -> p.thld));
+
+        Trie trie = new Trie();
+
+        int i = 0; 
+        for(query a : qq){
+            while(i < n && nums[i] <= a.thld){
+                trie.insert(nums[i]);
+                i++;
+            }
+
+            if(i != 0){
+                ans[a.idx] = trie.findMax(a.val);
+            }else{
+                ans[a.idx] = -1;
+            }
+        }
+        return ans;
+    }
+}
+
+class query{
+    int val;
+    int thld;
+    int idx;
+    query(int val , int thld , int idx){
+        this.val = val;
+        this.thld = thld;
+        this.idx = idx;
+    }
+}
+
+
+/*class Node{
     Node[] links;
 
     Node(){
@@ -93,4 +192,4 @@ class Query{
         this.threshold = threshold;
         this.index = index;
     }
-}
+} */
