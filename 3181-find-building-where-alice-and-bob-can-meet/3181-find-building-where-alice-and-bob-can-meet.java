dcd0aa9b6e82ecc8b70b1c4821a57236028cddee
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-05-21 23:03:55 +0530

    Time: 772 ms (5.49%), Space: 67.5 MB (93.25%) - LeetHub

--- 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.java
+++ 3181-find-building-where-alice-and-bob-can-meet/3181-find-building-where-alice-and-bob-can-meet.java
@@ -1,34 +1,94 @@
+//TC == O(q * logN * logN)
 class Solution {
-       public int[] leftmostBuildingQueries(int[] A, int[][] queries) {
-        int n = A.length, qn = queries.length;
-        List<int[]>[] que = new ArrayList[n];
-        for (int i = 0; i < n; i++)
-            que[i] = new ArrayList();
-        PriorityQueue<int[]> h = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        int[] res = new int[qn];
-        Arrays.fill(res, -1);
-        // Step 1
-        for (int qi = 0; qi < qn; qi++) {
-            int i = queries[qi][0], j = queries[qi][1];
-            if (i < j && A[i] < A[j]) {
-                res[qi] = j;
-            } else if (i > j && A[i] > A[j]) {
-                res[qi] = i;
-            } else if (i == j) {
-                res[qi] = i;
-            } else { // Step 2
-                que[Math.max(i, j)].add(new int[]{Math.max(A[i], A[j]), qi});
-            }
+    void buildST(int i, int l, int r, int ST[], int heights[]) {
+        if (l == r) {
+            ST[i] = l;
+            return;
         }
-        // Step 3
-        for (int i = 0; i < n; i++) {
-            while (!h.isEmpty() && h.peek()[0] < A[i]) {
-                res[h.poll()[1]] = i;
-            }
-            for (int[] q : que[i]) {
-                h.add(q);
-            }
+        int mid = l + (r - l) / 2;
+        buildST(2 * i + 1, l, mid, ST, heights);
+        buildST(2 * i + 2, mid + 1, r, ST, heights);
+        int li = ST[2 * i + 1];
+        int ri = ST[2 * i + 2];
+        if (heights[li] >= heights[ri]) {
+            ST[i] = li;
+        } else {
+            ST[i] = ri;
         }
+    }
 
-        return res;
-    }}
+    int[] CT(int heights[], int n) {
+        int ST[] = new int[4 * n];
+        buildST(0, 0, n - 1, ST, heights);
+        return ST;
+    }
+
+    int query(int start, int end, int i, int l, int r, int ST[], int heights[]) {
+        if (l > end || r < start)
+            return -1;
+        if (l >= start && r <= end)
+            return ST[i];
+
+        int mid = l + (r - l) / 2;
+
+        int left = query(start, end, 2 * i + 1, l, mid, ST, heights);
+        int right = query(start, end, 2 * i + 2, mid + 1, r, ST, heights);
+
+        if (left == -1)
+            return right;
+        if (right == -1)
+            return left;
+        if (heights[left] >= heights[right]) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+
+    int RMIQ(int ST[], int heights[], int n, int start, int end) {
+        return query(start, end, 0, 0, n - 1, ST, heights);
+    }
+
+    public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
+        int n = heights.length;
+        int q = queries.length;
+
+        int ST[] = CT(heights, n);
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++)
+            maxi = Math.max(maxi, heights[i]);
+
+        int ans[] = new int[q];
+        Arrays.fill(ans, -1);
+        for (int i = 0; i < q; i++) {
+            int l = Math.min(queries[i][0], queries[i][1]);
+            int r = Math.max(queries[i][0], queries[i][1]);
+
+            if (l == r || heights[r] > heights[l]) {
+                ans[i] = r;
+                continue;
+            }
+            // for(int j = Math.max(l , r) ; j < n ; j++){
+            //     if(heights[j] > heights[l] && heights[j] > heights[r]){
+            //         ans[i] = j;
+            //         break;
+            //     }
+            // }
+
+            int left = r + 1, right = n - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                int idx = RMIQ(ST, heights, n, left, mid);
+                if (heights[idx] > Math.max(heights[l], heights[r])) {
+                    ans[i] = idx;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+        }
+        return ans;
+    }
+}
