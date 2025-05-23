30990d83b42f787db004c4c85f070ce249f93199
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-05-24 01:12:54 +0530

    Time: 8 ms (34.51%), Space: 54.9 MB (27.46%) - LeetHub

---
+++ 1359-circular-permutation-in-binary-representation/1359-circular-permutation-in-binary-representation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> circularPermutation(int p, int start) {
+        int n = (int)Math.pow(2 , p);
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
+            list.add(i ^ (i >> 1));
+        }
+        List<Integer> ans = new ArrayList<>();
+        boolean flag = false;
+        int k = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(list.get(i) == start){
+                k = i;
+                flag = true;
+            }
+            if(flag) ans.add(list.get(i));
+            else continue;
+        }
+        for(int i = 0 ; i < k ; i++){
+            ans.add(list.get(i));
+        }
+        return ans;
+    }
+}
