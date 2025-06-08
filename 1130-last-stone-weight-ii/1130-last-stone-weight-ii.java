e27d4777db7d3424a69d9dbf80de1cae157c7852
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-06-08 18:36:32 +0530

    Time: 2 ms (93.02%), Space: 41.3 MB (67.09%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.java
@@ -1,6 +1,25 @@
 class Solution {
     public int lastStoneWeightII(int[] nums) {
         int n = nums.length;
+        int total = 0 ;
+        for(int num : nums) total += num;
+        boolean dp[] = new boolean[total / 2 + 1];
+        dp[0] = true;
+        
+        for(int x : nums){
+            for(int sum = total / 2 ; sum >= x ; sum--){
+                dp[sum] = dp[sum] || dp[sum - x];
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i = total / 2 ; i >= 0; i--) if(dp[i]) return total - 2 * i;
+        return 0;
+    }
+}
+
+/*class Solution {
+    public int lastStoneWeightII(int[] nums) {
+        int n = nums.length;
         int sum  = 0;
         for(int x : nums) sum += x;
         int dp[][] = new int[n][sum + 1];
@@ -17,4 +36,4 @@ class Solution {
         int not_pick = helper(nums , idx + 1 , curr , sum, dp);
         return dp[idx][curr] =  Math.min(pick , not_pick);
     }
-}
+} */
