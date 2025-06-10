4d9c70525a5f66abd1b7762e9421c95afffb9d36
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-06-10 16:10:39 +0530

    Time: 3 ms (84.2%), Space: 49.5 MB (71.16%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean vis[][] = new boolean[n][m];
+
+        int numberOfIslands = 0;
+        
+        for(int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == '1' && vis[i][j] == false){
+                    helper(grid , i , j , vis);
+                    numberOfIslands++;
+                }
+            }
+        }
+        return numberOfIslands;
+    }
+
+    private void helper(char[][] grid , int i , int j , boolean[][] vis){
+        int n = grid.length;
+        int m = grid[0].length;
+
+        if(i < 0 || i >= n || j < 0 || j >= m || vis[i][j] == true || grid[i][j] == '0') return;
+        vis[i][j] = true;
+
+        helper(grid , i - 1 , j , vis);
+        helper(grid , i , j + 1, vis);
+        helper(grid , i + 1 , j , vis);
+        helper(grid , i , j - 1, vis);
+
+    }
+}
