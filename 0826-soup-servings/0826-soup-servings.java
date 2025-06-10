cb4d2e9a863d10afe0acf424aef348e351c993ca
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-06-10 12:30:04 +0530

    Time: 48 ms (28.89%), Space: 231.1 MB (7.78%) - LeetHub

---
+++ 0826-soup-servings/0826-soup-servings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double soupServings(int n) {
+        if(n >= 5000 ) return 1.0;
+        double dp[][] = new double[n + 1][n + 1];
+        for(double[] x : dp) Arrays.fill(x , -1.0);
+        return helper(n , n , dp);
+    }
+    private double helper(int a , int b , double[][] dp){
+        if(a <= 0 && b <= 0) return 0.5;
+        if(a <= 0 ) return 1.0;
+        if(b <= 0 ) return 0.0;
+        if(dp[a][b] != -1.0) return dp[a][b];
+        double cnt = 0.0;
+        cnt += (helper(a - 100 , b , dp));
+        cnt += (helper(a - 75 , b - 25 , dp)); 
+        cnt += (helper(a - 50 , b - 50 , dp)); 
+        cnt += (helper(a - 25 , b - 75 , dp)); 
+        return dp[a][b] = 0.25 * cnt;
+    }
+}
