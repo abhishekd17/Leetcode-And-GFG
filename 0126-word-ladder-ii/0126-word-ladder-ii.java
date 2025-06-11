68be0feba4a7cca4e8c04f4ea51921af1c72c3ea
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-06-11 10:40:07 +0530

    Time: 23 ms (14.03%), Space: 45.4 MB (64.45%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.java
+++ 0126-word-ladder-ii/0126-word-ladder-ii.java
@@ -1,77 +1,68 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>(wordList);
-        List<List<String>> result = new ArrayList<>();
+        Queue<String> q = new LinkedList<>();
+        Map<String , List<String>> pre = new HashMap<>();
+        Map<String , Integer> visitedLevels = new HashMap<>();
+        Set<String> set = new HashSet<>(wordList);
+        List<List<String>> ans = new ArrayList<>();
 
-        if (!wordSet.contains(endWord)) return result;
+        // first case , if endword in not present in set
+        if(!set.contains(endWord)) return ans;
 
-        // Map to store predecessors
-        Map<String, List<String>> predecessors = new HashMap<>();
-        Map<String, Integer> visitedLevels = new HashMap<>();
-
-        Queue<String> queue = new LinkedList<>();
-        queue.add(beginWord);
-        visitedLevels.put(beginWord, 0);
+        q.add(beginWord);
+        visitedLevels.put(beginWord , 0);
 
         int level = 0;
         boolean foundEndWord = false;
 
-        while (!queue.isEmpty() && !foundEndWord) {
-            int size = queue.size();
+        while(!q.isEmpty()){
+            int size = q.size();
             level++;
             Set<String> visitedThisLevel = new HashSet<>();
 
-            for (int i = 0; i < size; i++) {
-                String word = queue.poll();
-
-                char[] arr = word.toCharArray();
-                for (int j = 0; j < arr.length; j++) {
+            for(int i = 0 ; i < size ; i++){
+                String word = q.poll();
+                char arr[] = word.toCharArray();
+                for(int j = 0 ; j < arr.length ; j++){
                     char original = arr[j];
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        arr[j] = c;
+                    for(char ch ='a' ; ch <= 'z' ; ch++){
+                        arr[j] = ch;
                         String nextWord = new String(arr);
+                        if(!set.contains(nextWord)) continue;
 
-                        if (!wordSet.contains(nextWord)) continue;
-
-                        if (!visitedLevels.containsKey(nextWord)) {
+                        if(!visitedLevels.containsKey(nextWord)){
                             visitedThisLevel.add(nextWord);
-                            queue.add(nextWord);
-                            visitedLevels.put(nextWord, level);
+                            q.add(nextWord);
+                            visitedLevels.put(nextWord , level);
                         }
 
-                        if (visitedLevels.get(nextWord) == level) {
-                            predecessors.computeIfAbsent(nextWord, k -> new ArrayList<>()).add(word);
+                        if(visitedLevels.get(nextWord) == level){
+                            pre.computeIfAbsent(nextWord , k -> new ArrayList<>()).add(word);
                         }
 
-                        if (nextWord.equals(endWord)) {
-                            foundEndWord = true;
-                        }
+                        if(nextWord.equals(endWord)) foundEndWord = true;
                     }
                     arr[j] = original;
                 }
             }
         }
 
-        if (foundEndWord) {
+        if(foundEndWord){
             List<String> path = new ArrayList<>();
-            backtrack(endWord, beginWord, predecessors, path, result);
+            backtrack(endWord , beginWord , pre , path , ans);
         }
-
-        return result;
+        return ans;
     }
 
-    private void backtrack(String current, String beginWord, Map<String, List<String>> predecessors,
-                           List<String> path, List<List<String>> result) {
-        path.add(current);
-        if (current.equals(beginWord)) {
+    private void backtrack(String curr , String beginWord ,Map<String , List<String>> pre, List<String> path ,List<List<String>> ans){
+        path.add(curr);
+        if(curr.equals(beginWord)){
             List<String> temp = new ArrayList<>(path);
             Collections.reverse(temp);
-            result.add(temp);
-        } else if (predecessors.containsKey(current)) {
-            for (String prev : predecessors.get(current)) {
-                backtrack(prev, beginWord, predecessors, path, result);
+            ans.add(temp);
+        }else if(pre.containsKey(curr)){
+            for(String p : pre.get(curr)){
+                backtrack(p , beginWord , pre , path , ans);
             }
         }
         path.remove(path.size() - 1);
