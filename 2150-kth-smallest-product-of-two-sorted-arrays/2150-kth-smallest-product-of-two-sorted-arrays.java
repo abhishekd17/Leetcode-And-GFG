bc238ae2a5e9e1fdcb774d789d5ee246adade076
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-06-26 01:52:17 +0530

    Time: 725 ms (50.96%), Space: 56 MB (8.05%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,43 +1,57 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        long left = -10000000000L;
-        long right = 10000000000L;
-        
-        while (left < right) {
-            long mid = left + (right - left) / 2;
-            if (countProducts(nums1, nums2, mid) < k) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+        int n = nums1.length , m = nums2.length;
+        long l = -10000000000L;
+        long r = 10000000000L;
+
+        while(l < r){
+            long mid = l + (r - l) / 2;
+            if(helper(mid , nums1 , nums2) >= k ) r = mid;
+            else l = mid + 1;
         }
-        
-        return left;
+        return l;
     }
-    
-    private long countProducts(int[] nums1, int[] nums2, long target) {
-        long count = 0;
-        for (int num1 : nums1) {
-            if (num1 == 0) {
-                if (target >= 0) count += nums2.length;
+
+    private long helper(long tar , int nums1[] , int nums2[]){
+        long cnt = 0;
+        for(int num : nums1){
+            if(num == 0){
+                if(tar >= 0) cnt += nums2.length;
                 continue;
             }
-            
-            int low = 0, high = nums2.length;
-            while (low < high) {
-                int mid = low + (high - low) / 2;
-                long product = (long) num1 * nums2[mid];
-                if (product <= target) {
-                    if (num1 > 0) low = mid + 1;
-                    else high = mid;
-                } else {
-                    if (num1 > 0) high = mid;
-                    else low = mid + 1;
+
+            int l = 0 , r = nums2.length;
+            while(l < r){
+                int mid = l + (r - l) / 2;
+                long pro = (long) num * nums2[mid];
+                if(pro <= tar){
+                    if(num < 0) r = mid;
+                    else l = mid + 1;
+                }else{
+                    if(num > 0) r = mid;
+                    else l = mid + 1;
                 }
             }
-            
-            count += (num1 > 0) ? low : nums2.length - low;
+
+            cnt += (num > 0) ? l :  nums2.length - l;
         }
-        return count;
+        return cnt;
     }
 }
+
+/*class Solution {
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        int n = nums1.length , m = nums2.length;
+        PriorityQueue<Long> pq = new PriorityQueue<>((a,b) -> Long.compare(b,a));
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                long x = (long)nums1[i] * nums2[j];
+                if(pq.size() >= k && pq.peek() > x) pq.poll();
+                if(pq.size() < k ) pq.offer(x);
+                System.out.println(pq.peek());
+            }
+        }
+        return pq.peek();
+    }
+} */
