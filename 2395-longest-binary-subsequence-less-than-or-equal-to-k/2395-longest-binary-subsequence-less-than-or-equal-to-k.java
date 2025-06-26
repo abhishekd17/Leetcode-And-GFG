aa7f78605d698e655430e4bd4c14310aba285126
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-06-26 21:06:41 +0530

    Time: 1 ms (100%), Space: 41.9 MB (67.02%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.java
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.java
@@ -1,25 +1,20 @@
 class Solution {
     public int longestSubsequence(String s, int k) {
         int n = s.length();
-        int[][] dp = new int[n][n + 1]; 
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return helper(n - 1, 0, k, s, dp);
-    }
-
-    private int helper(int ind, int size, int sum, String s, int[][] dp) {
-        if (ind < 0) return 0;
-        if (dp[ind][size] != -1) return dp[ind][size];
-
-        int np = helper(ind - 1, size, sum, s, dp);
-
-        int p = 0;
-        int bit = s.charAt(ind) - '0';
-
-        if (bit == 0 || (size < 31 && (bit << size) <= sum)) {
-            int val = bit << size;
-            p = 1 + helper(ind - 1, size + 1, sum - val, s, dp);
+        long val = 0;
+        int cnt = 0;
+        int power = 0;
+        for(int i = n - 1 ; i >= 0 ; i--){
+            char ch = s.charAt(i);
+            if(ch == '0') cnt++;
+            else{
+                if(power < 32 && (val + (long) (1L << power)) <= k){
+                    val += (1L << power);
+                    cnt++;
+                }
+            }
+            power++;
         }
-
-        return dp[ind][size] = Math.max(p, np);
+        return cnt;
     }
 }
