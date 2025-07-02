6ff6fe074f97ba2b0ed23cd6b67faa0e39c5cbe5
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-07-02 19:15:11 +0530

    Time: 2 ms (97.45%), Space: 43.5 MB (99.56%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -5,36 +5,32 @@ class Solution {
         }
         return true;
     }
-    public boolean isMatch(String s, String p) {
-        int n = s.length();
-        int m = p.length();
-        boolean prev[] =  new boolean[m + 1];
-        prev[0] = true;
+    public boolean isMatch(String a, String b) {
+        int n = a.length(), m = b.length();
+        int i = 0, j = 0, star = -1, match = 0;
 
-        for(int i = 1 ; i <= m ; i++){
-            if(isAllStars(p , i - 1)){
-                prev[i] = true;
-            }else{
-                prev[i] = false;
+        while (i < n) {
+            if (j < m && (b.charAt(j) == a.charAt(i) || b.charAt(j) == '?')) {
+                i++;
+                j++;
+            } else if (j < m && b.charAt(j) == '*') {
+                star = j;
+                match = i;
+                j++;
+            } else if (star != -1) {
+                j = star + 1;
+                match++;
+                i = match;
+            } else {
+                return false;
             }
         }
 
-        for(int i = 1 ; i <= n ;i++){
-            boolean curr[] = new boolean[m + 1];
-            for(int j = 1 ; j <= m ;j++){
-               if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?'){
-                    curr[j] = prev[j - 1];
-                }else{
-                    if(p.charAt(j - 1) == '*'){
-                        curr[j] = (prev[j] || curr[j - 1]);
-                    }else{
-                        curr[j] = false;
-                    }
-                } 
-            }
-            prev = curr;
+        while (j < m && b.charAt(j) == '*') {
+            j++;
         }
-        return prev[m];
+
+        return j == m;
     }
 }
 
