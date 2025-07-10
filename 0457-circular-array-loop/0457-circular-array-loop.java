59a71fe9b2b5e8db8c96ea66c2e3b4fbd097ea56
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-07-10 14:47:20 +0530

    Time: 0 ms (100%), Space: 40.9 MB (94.57%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -3,22 +3,36 @@ class Solution {
         int n = nums.length;
 
         for(int i = 0 ; i < n ; i++){
-            boolean vis[] = new boolean[n];
-            for(int j = i ; ;){
-                if(vis[j]) return true;
-                vis[j] = true;
-                if(nums[j] > 0){
-                    int x = (j + nums[j]) % n;
-                    if(j == x || nums[x] < 0) break;
-                    j  = x;
-                }
-                else{
-                    int x = ((j + nums[j]) % n + n) % n;
-                    if(j == x || nums[x] > 0) break;
-                    j = x;
-                }
+            if(nums[i] == 0) continue;
+            int slow = i , fast = i;
+            boolean isForward = nums[i] > 0;
+
+            while(true){
+                slow = f(nums , slow , isForward);
+                fast = f(nums , fast , isForward);
+                if(fast != -1) fast = f(nums , fast , isForward);
+                if(slow == -1 || fast == -1 || slow == fast) break;
+            }
+
+            if(slow != -1 && slow == fast) return true;
+
+            int ind = i;
+            while(f(nums , ind , isForward) != -1){
+                int next = f(nums , ind , isForward);
+                nums[ind] = 0;
+                ind = next;
             }
         }
+
         return false;
+    }
+
+    private int f(int nums[] , int i , boolean isForward){
+        boolean dir = nums[i] > 0;
+        int n = nums.length;
+        if(dir != isForward || nums[i] % n == 0) return -1;
+        int next = ((i + nums[i]) % n + n) % n;
+        if(next == i) return -1;
+        return next; 
     }
 }
