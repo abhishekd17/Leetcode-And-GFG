45e2b9474c34aaf2d694d60df28a9bf0199a3650
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-07-17 15:38:05 +0530

    Time: 5 ms (60.48%), Space: 63.4 MB (13.71%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maximumLength(int[] nums) {
-        int c = nums[0] % 2, odd = 0, even = 0, both = 0;
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                even++;
-            } else {
-                odd++;
-            }
-            if (num % 2 == c) {
+        int n = nums.length;
+        int parity = nums[0] % 2 , even = 0 , odd = 0, both = 0;
+        for(int num : nums){
+            if(num % 2 == 0) even++;
+            else odd++;
+            if(num % 2 == parity){
                 both++;
-                c = 1 - c;  // Toggle the parity
+                parity = 1 - parity;
             }
         }
-        return Math.max(both, Math.max(even, odd));
+        return Math.max(both , Math.max(even , odd));
     }
 }
