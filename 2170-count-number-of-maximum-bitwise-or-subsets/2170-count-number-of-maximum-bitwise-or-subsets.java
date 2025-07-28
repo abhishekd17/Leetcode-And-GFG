d275a2406d43fe2a9975e8580401a18243ae3b5c
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-07-28 16:50:40 +0530

    Time: 14 ms (32.62%), Space: 44.6 MB (24.73%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,12 +1,29 @@
 class Solution {
-        public int countMaxOrSubsets(int[] A) {
-        int max = 0, dp[] = new int[1 << 17];
-        dp[0] = 1;
-        for (int a : A) {
-            for (int i = max; i >= 0; --i)
-                dp[i | a] += dp[i];
-            max |= a;
+    public int countMaxOrSubsets(int[] nums) {
+        int n = nums.length;
+        
+        int maxi = 0;
+        for(int x : nums) maxi |= x;
+        
+        Map<String ,  Integer> dp = new HashMap<>();
+        
+        return helper(nums , 0 , 0 , maxi , dp);
+    }
+
+    private int helper(int nums[] , int ind , int currOr , int maxi , Map<String ,  Integer> dp){
+        if(ind == nums.length){
+            return maxi == currOr ? 1 : 0; 
         }
-        return dp[max];
+        
+        String key = ind + "$" + currOr;
+        if(dp.containsKey(key)) return dp.get(key);
+        
+        int np = helper(nums , ind + 1 , currOr , maxi , dp);
+        int p = helper(nums , ind + 1 , currOr | nums[ind] , maxi , dp);
+        
+        int res = p + np;
+        dp.put(key , res);
+        
+        return res;
     }
 }
