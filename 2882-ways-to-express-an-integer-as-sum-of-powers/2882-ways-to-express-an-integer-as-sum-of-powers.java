5d1e8b0483074d8609b0468d5d797c552c924058
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2025-08-12 22:42:36 +0530

    Time: 71 ms (65.79%), Space: 45.2 MB (28.95%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,5 +1,29 @@
 class Solution {
     final int mod = 1000000007;
+    public int numberOfWays(int n, int x) {
+        int dp[][] = new int[n + 1][n + 1];
+        // first row --> 1
+        for(int i = 0 ; i <= n ; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int curr = 1 ; curr <= n ; curr++){
+            int power = (int) Math.pow(curr , x);
+            for(int rem = 0 ; rem <= n ; rem++){
+                if(power > rem){
+                    dp[curr][rem] = dp[curr - 1][rem];
+                }else{
+                    dp[curr][rem] = (dp[curr - 1][rem] + dp[curr - 1][rem - power]) % mod;
+                }
+            }
+        }
+        return dp[n][n];
+    }
+}
+
+
+/*class Solution {
+    final int mod = 1000000007;
     int p;
     public int numberOfWays(int n, int x) {
         p = n;
@@ -12,6 +36,7 @@ class Solution {
         if(rem == 0) return 1;
         if(rem < 0) return 0;
         if(curr > p ) return 0;
+        
         if(dp[curr][rem] != - 1) return dp[curr][rem];
 
         int power = (int) Math.pow(curr , x);
@@ -22,4 +47,4 @@ class Solution {
         
         return dp[curr][rem] = (pick + not_pick) % mod;
     }
-}
+} */
