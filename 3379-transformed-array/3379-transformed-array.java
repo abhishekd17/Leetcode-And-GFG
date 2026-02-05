eb1836820aafd03a5cd2190ed62535f6102520b5
Abhishek Kumawat <111186621+abhishekd17@users.noreply.github.com>
2026-02-05 23:57:13 +0530

    Time: 1 ms (98.72%), Space: 46.8 MB (51.92%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                result[i] = nums[i];
+            } 
+            else if (nums[i] > 0) {
+                int newIndex = (i + nums[i]) % n;
+                result[i] = nums[newIndex];
+            } 
+            else { // nums[i] < 0
+                int steps = Math.abs(nums[i]);
+                int newIndex = (i - steps) % n;
+                if (newIndex < 0) newIndex += n;
+                result[i] = nums[newIndex];
+            }
+        }
+        return result;
+    }
+}
